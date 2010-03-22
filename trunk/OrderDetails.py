bbf3fee9a229e7708940165134d26a3efbe51fed
peter <peter@gmail.com>
2010-03-22 15:35:26 +0000

    Data holder class; basically, holds the information for a single order without having to append it to the table.  Very possibly unnecessary in the end, but convenient for now. --Peter

---
+++ trunk/OrderDetails.py
@@ -0,0 +1,12 @@
+class OrderDetails:
+    """
+    This is simply a holder class to easily pass all necessary order information between methods
+    This could also be easily implemented as a type rather than a class
+    """
+    def __init__ (self,shares,symbol,orderType,duration,closeType,limitPrice = None):
+        self.shares = shares
+        self.symbol = symbol
+        self.orderType = orderType
+        self.duration = duration
+        self.closeType = closeType
+        self.limitPrice = limitPrice
