5c9e1376e224966414edcadd78ee89be5278d652
micahmyerscough <micahmyerscough@gmail.com>
2010-04-08 15:44:33 +0000

    Beta version to be released to students for strategy simulation.

---
+++ beta/models/StrategyDataModel.py
@@ -0,0 +1,92 @@
+import tables as pt #@UnresolvedImport
+import time
+    
+class StrategyDataModel(pt.IsDescription):
+    symbol = pt.StringCol(30)           #30 char string; Ticker
+    exchange = pt.StringCol(10)         #10 char string; NYSE, NASDAQ, etc.
+    adj_high = pt.Float32Col()
+    adj_low = pt.Float32Col()
+    adj_open = pt.Float32Col()
+    adj_close = pt.Float32Col()
+    close = pt.Float32Col()
+    volume = pt.Int32Col()
+    timestamp = pt.Time64Col()
+    when_available = pt.Time64Col()
+    interval = pt.Time64Col()
+
+def generateDataFile():
+    import random
+    random.seed(1)    
+    #86400 seconds in a day
+    stocks = []
+    for i in range(10): #stocks
+        stocks.append('stock%i'%i)
+    
+    h5f = pt.openFile('PriceTestData.h5', mode = "w")
+    group = h5f.createGroup("/", 'tester')
+    table = h5f.createTable(group, 'testTable', StockPriceModel)
+    row = table.row
+
+    for i in range(100): #timestamps
+        j = 0
+        for stock in stocks:
+            adjOpen = random.random() * random.randint(1,100)   
+            adjClose = random.random() * random.randint(1,100) 
+            row['exchange'] = 'NYSE'
+            row['symbol'] = stock
+            row['data/adj_open'] = adjOpen 
+            row['data/adj_close'] = adjClose
+            row['data/adj_high'] = max(adjOpen,adjClose) * random.randint(1,5)
+            row['data/adj_low'] = min(adjOpen,adjClose) / random.randint(1,5)
+            row['data/close'] = adjClose
+            row['data/volume'] = random.randint(1000,10000)
+            row['data/timestamp'] = i*86400
+            row['data/when_available'] = i*86400
+            row['data/interval'] = 86400         
+            row.append()
+            table.flush()
+        if i%10==0:
+            print i,
+        if i%100==0:
+            print ''
+    h5f.close()
+#generateDataFile()
+
+def showData():
+    h5f = pt.openFile('PriceTestData.h5', mode = "r")
+    table = h5f.root.tester.testTable
+    i = 0    
+    for row in table.iterrows():
+        if i<10:
+            print row['data/adj_close']
+        i+=1
+        
+def classTest():        
+    #populate data file
+    h5f = pt.openFile('StockPriceTest.h5', mode = "w")
+    group = h5f.createGroup("/", 'tester')
+    table = h5f.createTable(group, 'testTable', StockPriceModel)
+    row = table.row
+    for i in range(10):
+        if(i<2):
+            row['symbol'] = 'KO'
+        else:
+            row['symbol'] = 'AAPL'
+        row['exchange'] = 'NYSE'
+        row['timestamp'] = 1000+i*10
+        row['when_available'] = 1100+i*10
+        row['interval'] = 1000+i*10
+        row['data/adj_high'] = 25.32
+            
+        row.append()
+    h5f.close()
+    
+    #reopen and access data file
+    h5f = pt.openFile('StockPriceTest.h5', mode = "r")
+    table = h5f.root.tester.testTable
+        
+    #for row in table.iterrows():
+    #    print row['timestamp'], row['symbol'], row['exchange'],\
+    #    row['timestamp'], row['when_available'], row['interval'], row['data']
+    
+    h5f.close()
