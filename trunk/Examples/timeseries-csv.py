e1a75cecbfcd229e5f70c7e89b5af822633d156f
tucker777 <tucker@lucenaresearch.com>
2010-10-03 22:27:28 +0000

    Some example code and data for working with timeseries.

---
+++ trunk/Examples/timeseries-csv.py
@@ -0,0 +1,90 @@
+#
+# Some examples of how to manipulate stock price data in Python
+#
+# by Tucker Balch
+# September 2010
+#
+
+#
+# imports
+#
+import numpy as np
+import matplotlib.pyplot as plt
+from pylab import *
+import datetime as dt
+
+#
+# read in and slice up the data
+#
+data = np.loadtxt('example-data.csv',delimiter=',',skiprows=1)
+pricedat = data[:,3:]
+datesdat = np.int_(data[:,0:3]) # date stuff should be integer
+pricesnames = ['SPY','XOM','GOOG','GLD']
+
+print 'first 5 rows of price data:'
+print pricedat[:5,:]
+print
+print 'first 5 rows of dates:'
+print datesdat[:5,:]
+
+#
+# Convert columns of date info into date objects
+#
+dates = []
+for i in range(0,datesdat.shape[0]):
+	dates.append(dt.date(datesdat[i,0],datesdat[i,1],datesdat[i,2]))
+
+#
+# Plot the closing prices 
+#
+plt.clf()
+for i in range(0,size(pricedat[0,:])):
+	plt.plot(dates,pricedat[:,i])
+plt.legend(pricesnames)
+plt.ylabel('Adjusted Close')
+plt.xlabel('Date')
+savefig('adjustedclose.pdf', format='pdf')
+
+#
+# Plot the normalized data
+#
+plt.clf()
+normdat = pricedat/pricedat[0,:]
+for i in range(0,size(normdat[0,:])):
+	plt.plot(dates,normdat[:,i])
+plt.legend(pricesnames)
+plt.ylabel('Normalized Adjusted Close')
+plt.xlabel('Date')
+savefig("normalizedclose.pdf", format='pdf')
+
+#
+# Daily returns
+#
+plt.clf()
+dailyrets = concatenate(([(zeros(pricedat.shape[1]))],
+	((pricedat[1:,:]/pricedat[0:-1,:]) - 1)),axis=0)
+plt.plot(dates[0:49],dailyrets[0:49,0]) # just plot first 50 days
+plt.plot(dates[0:49],dailyrets[0:49,1])
+plt.axhline(y=0,color='r')
+plt.legend(['SPY','XOM'])
+plt.ylabel('Daily Returns')
+plt.xlabel('Date')
+savefig("dailyrets.pdf", format='pdf')
+
+#
+# scatter plot SPY v XOM
+#
+plt.clf()
+plt.scatter(dailyrets[:,0],dailyrets[:,1],c='blue')
+plt.ylabel('XOM')
+plt.xlabel('SPY')
+savefig("scatterSPYvXOM.pdf", format='pdf')
+
+#
+# scatter plot SPY v GLD
+#
+plt.clf()
+plt.scatter(dailyrets[:,0],dailyrets[:,3],c='red')
+plt.ylabel('GLD')
+plt.xlabel('SPY')
+savefig("scatterSPYvGLD.pdf", format='pdf')
