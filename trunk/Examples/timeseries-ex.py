960cab94bfc2a635e1844e57e593e3fdab60a26a
tucker777 <tucker@lucenaresearch.com>
2010-10-07 14:59:15 +0000

    new

---
+++ trunk/Examples/timeseries-ex.py
@@ -0,0 +1,40 @@
+import matplotlib.pyplot as plt
+from pylab import *
+from qstkutil import DataAccess as da
+from qstkutil import timeutil as tu
+import timeseries as ts
+
+# Set the list of stocks for us to look at
+symbols= list()
+symbols = list(np.loadtxt('example-syms.csv',dtype='str',delimiter=',',
+	comments='#',skiprows=0))
+symbols.append("IBM")
+#symbols.append("BLAH")  # uncomment this line to see what happens
+
+# Set start and end boundary times.  They must be specified in Unix Epoch
+tsstart = tu.ymd2epoch(2008,1,1)
+tsend = tu.ymd2epoch(2010,1,1)
+
+# Get the data from the data store
+adjcloses = ts.getTSFromData("Norgate","open",symbols,tsstart,tsend)
+
+print "The adjusted closing prices are: "
+print adjcloses.values
+
+# Convert the timestamps to dates
+dates = []
+for ts in adjcloses.timestamps:
+    dates.append(tu.epoch2date(ts))
+
+# Normalize the prices
+normdat = adjcloses.values/adjcloses.values[0,:]
+
+# Plot the closing prices
+plt.clf()
+for i in range(0,size(normdat[0,:])):
+        plt.plot(dates,normdat[:,i])
+plt.legend(symbols)
+plt.ylabel('Adjusted Close')
+plt.xlabel('Date')
+plt.draw()
+savefig("fig1.pdf", format='pdf')
