df2244dec830d83099a165e178a2adfee4ad4084
tucker777 <tucker@lucenaresearch.com>
2010-10-29 06:26:06 +0000

    updated eventstocks.csv

--- trunk/Examples/names.py
+++ trunk/Examples/names.py
@@ -17,15 +17,22 @@ symbols = list(np.loadtxt('allsyms.csv',dtype='str',delimiter=',',
 
 # Set start and end boundary times.  They must be specified in Unix Epoch
 tsstart = tu.ymd2epoch(2008,1,1)
-tsend = tu.ymd2epoch(2009,1,1)
+tsend = tu.ymd2epoch(2008,12,31)
 
 # Get the data from the data store
 storename = "Norgate" # get data from our daily prices source
 fieldname = "close" # adj_open, adj_close, adj_high, adj_low, close, volume
 closes = ps.getDataMatrixFromData(storename,fieldname,symbols,tsstart,tsend)
 
-print "data read"
-
 cldata = closes.values
-cldata[cldata>5.0] = NaN
-cldata[cldata<=5.0] = 1.0
+cldata[cldata<=4.0]  = 1.0
+cldata[cldata>4.0]   = 0
+cldata[isnan(cldata)]= 0
+lows = sum(cldata,axis=0)
+
+lownames = array(closes.cols())[lows>0]
+
+f = open('below4.csv', 'w')
+for sym in lownames:
+	f.write(sym + '\n')
+f.close()
