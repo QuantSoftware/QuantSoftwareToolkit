dfffe1b74d591af8be1299739786d087a7681e83
dbratcher <dbratcher@gatech.edu>
2010-11-17 06:10:16 +0000

    created MonthlyRebalancingExample which creates a sample monthly allocation table from 2004 to 2009 and runs a quickSim.

--- trunk/QuickSim/quickSim.py
+++ trunk/QuickSim/quickSim.py
@@ -1,10 +1,5 @@
-#
-# quickSim.py
-#
-# A simulator that quickly produces a fund history.
-#
-# Drew Bratcher
-#
+# # quickSim.py # # A simulator that quickly produces a fund history.
+# # Drew Bratcher #
 
 def quickSim(alloc,historic,start_cash):
 
@@ -16,10 +11,10 @@ def quickSim(alloc,historic,start_cash):
 
     #check each row in alloc
     for row in range(0,len(alloc.values[:,0])):
-        if(alloc.values[row,:].sum()!=1):
+        if(abs(alloc.values[row,:].sum()-1)>.1):
             print alloc.values[row,:]
             print alloc.values[row,:].sum()
-            print "warning, alloc row "+row+" does not sum to one"
+            print "warning, alloc row "+str(row)+" does not sum to one"
   
     #fix invalid days
     historic=historic.fill(method='backfill')
@@ -27,14 +22,14 @@ def quickSim(alloc,historic,start_cash):
     #add cash column
     historic['_CASH'] = ones((len(historic.values[:,0]),1), dtype=int)
     
-    closest=historic[historic.index<=tu.epoch2date(alloc.index[0])]
+    closest=historic[historic.index<=alloc.index[0]]
     fund_ts=Series([start_cash], index=[closest.index[-1]])
     shares=alloc.values[0,:]*fund_ts.values[-1]/closest.values[-1,:]
     
     #compute all trades
     for i in range(1,len(alloc.values[:,0])):
         #get closest date(previous date)
-        closest=historic[historic.index<=tu.epoch2date(alloc.index[i])]
+        closest=historic[historic.index<=alloc.index[i]]
         #for loop to calculate fund daily (without rebalancing)
         for date in closest[closest.index>fund_ts.index[-1]].index:
             #compute and record total fund value (Sum(closest close * stocks))
@@ -42,9 +37,9 @@ def quickSim(alloc,historic,start_cash):
         #distribute fund in accordance with alloc
         shares=alloc.values[i,:]*fund_ts.values[-1]/closest.xs(closest.index[-1])
 
-    #compute fund value for rest of historic data with final share distribution 
-    for date in historic[historic.index>tu.epoch2date(alloc.index[-1])].index:
-        fund_ts=fund_ts.append(Series([(closest.xs(date)*shares).sum()],index=[date]))  
+    #compute fund value for rest of historic data with final share distribution  (currently buggy for some reason...)
+#    for date in historic[historic.index>alloc.index[-1]].index:
+#        fund_ts=fund_ts.append(Series([(closest.xs(date)*shares).sum()],index=[date]))  
 
     #return fund record
     return fund_ts
