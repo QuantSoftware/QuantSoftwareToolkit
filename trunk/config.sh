4ea6615283623c8fc9151e5c8aad3050e7347d88
tucker777 <tucker@lucenaresearch.com>
2011-03-14 21:20:55 +0000

    revising source directory structure

--- trunk/config.sh
+++ trunk/config.sh
@@ -1,17 +1,34 @@
 #!/bin/bash
 #
-# Configuration script to set QuantSoftware enviroment variables
+###########
+# Configuration script to set QuantSoftware enviroment variables.
+# Edit the first few uncommented lines below, then copy this file 
+# to localconfig.sh. Be sure to "source localconfig.sh" each time 
+# you use QSTK
 #
 # Tucker Balch
 #
 
+###########
+# In most cases you should only have to change the lines regarding
+# QS and QSDATA.The rest are taken care of automatically.  However, 
+# if you run in to problems, double check again here.
 #
-# In most cases you should only have to change these variables.
-# The rest are taken care of automatically.  However, if you run
-# in to problems, double check again here.
-#
+
+# Where is your installation of QSTK? If you followed instructions on
+# on the wiki, this line should be correct:
 export QS=$HOME/QSTK
-export QSDATA=/hzr71/research/QSData
+
+# Where is your data. To use the default data that comes with the
+# distribution, use the first line (default).  If you are using the 
+# machines at GT use the second line (with hzr71)
+export QSDATA=$QS/QSData
+#export QSDATA=/hzr71/research/QSData
+
+###########
+# You probably should not need to revise any of the lines below.
+# 
+
 # Which machine are we on?
 export HOSTNAME=`hostname`
 
@@ -31,4 +48,3 @@ export PATH=$PATH:$QSBIN
 export REMOTEUSER=tb34
 export REMOTEHOST=gekko.cc.gatech.edu
 export REMOTEHOME=/nethome/$REMOTEUSER
-
