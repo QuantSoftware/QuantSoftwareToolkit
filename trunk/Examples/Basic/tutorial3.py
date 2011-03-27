c0564ed238f23958860cef1542fdd064093657fc
tucker777 <tucker@lucenaresearch.com>
2011-03-27 23:34:55 +0000

    tutorials

---
+++ trunk/Examples/Basic/tutorial3.py
@@ -0,0 +1,94 @@
+'''
+March 26, 2011
+
+@author: Tucker Balch and Shreyas Joshi
+@summary: An example to show how dataAccess works.
+'''
+
+import matplotlib.pyplot as plt
+from pylab import *
+from qstkutil import DataAccess as da
+from qstkutil import dateutil as du
+from qstkutil import returns as qr
+import datetime as dt
+
+#
+# Read the definition of the portfolio from a csv file
+#
+portfolio = np.loadtxt('tutorial3portfolio.csv',
+	dtype='S5,f4',
+	delimiter=',',
+        comments='#',
+	skiprows=1,
+	)
+print portfolio
+# Sort by symbol name
+portfolio = sorted(portfolio, key=lambda x: x[0])
+print portfolio
+
+#
+# Create two lists: portfolio names and allocations
+#
+portsyms = []
+portalloc = []
+for i in portfolio:
+	portsyms.append(i[0])
+	portalloc.append(i[1])
+
+#
+# Read in the symbols available and compare with our portfolio
+#
+dataobj = da.DataAccess('Norgate')
+all_symbols = dataobj.get_all_symbols()
+intersectsyms = list(set(all_symbols) & set(portsyms)) # valid symbols
+badsyms = []
+if size(intersectsyms)<size(portsyms):
+	badsyms = list(set(portsyms) - set(intersectsyms))
+	print "warning: portfolio contains symbols that do not exist:" 
+	print badsyms
+for i in badsyms: # remove the bad symbols from our portfolio
+	index = portsyms.index(i)
+	portsyms.pop(index)
+	portalloc.pop(index)
+
+#
+# Read the historical data in from our data store
+#
+endday = dt.datetime(2011,1,1) 
+startday = endday - dt.timedelta(days=1095) #three years back
+timeofday=dt.timedelta(hours=16)
+print "start getNYSEdays"
+timestamps = du.getNYSEdays(startday,endday,timeofday)
+print "start read"
+close = dataobj.get_data(timestamps,portsyms,"close")
+print "end read"
+
+#
+# Copy, prep, and compute daily returns
+#
+rets = close.values.copy()
+qr.fillforward(rets)
+qr.returnize0(rets)
+
+#
+# Estimate portfolio total returns
+#
+portrets = sum(rets*portalloc,axis=1)
+porttot = cumprod(portrets+1)
+componenttot = cumprod(rets+1,axis=0) # compute returns for components
+
+#
+# Plot the result
+#
+plt.clf()
+fig = plt.figure()
+fig.add_subplot(111)
+plt.plot(timestamps,componenttot,alpha=0.4)
+plt.plot(timestamps,porttot)
+names = portsyms
+names.append('portfolio')
+plt.legend(names)
+plt.ylabel('Cumulative Returns')
+plt.xlabel('Date')
+fig.autofmt_xdate(rotation=45)
+savefig('tutorial3.pdf',format='pdf')
