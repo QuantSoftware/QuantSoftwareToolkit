a319db96adf9a46d43a0ff91a93b0c0ba36c61a1
tucker777 <tucker@lucenaresearch.com>
2011-03-27 16:30:47 +0000

    set up for tutorials

---
+++ trunk/Examples/Basic/tutorial1.py
@@ -0,0 +1,68 @@
+import qstkutil.dateutil as du
+import qstkutil.returns as ret
+import qstkutil.DataAccess as da
+import datetime as dt
+import matplotlib.pyplot as plt
+from pylab import *
+
+#
+# Prepare to read the data
+#
+symbols = ["AAPL","GLD","GOOG","SPY","XOM"]
+startday = dt.datetime(2008,1,1)
+endday = dt.datetime(2009,12,31)
+timeofday=dt.timedelta(hours=16)
+timestamps = du.getNYSEdays(startday,endday,timeofday)
+
+dataobj = da.DataAccess('Norgate')
+voldata = dataobj.get_data(timestamps, symbols, "volume")
+close = dataobj.get_data(timestamps, symbols, "close")
+actualclose = dataobj.get_data(timestamps, symbols, "actual_close")
+
+#
+# Plot the adjusted close data
+#
+plt.clf()
+newtimestamps = close.index
+pricedat = close.values # pull the 2D ndarray out of the pandas object
+plt.plot(newtimestamps,pricedat)
+plt.legend(symbols)
+plt.ylabel('Adjusted Close')
+plt.xlabel('Date')
+savefig('adjustedclose.pdf',format='pdf')
+
+#
+# Plot the normalized closing data
+#
+plt.clf()
+normdat = pricedat/pricedat[0,:]
+plt.plot(newtimestamps,normdat)
+plt.legend(symbols)
+plt.ylabel('Normalized Close')
+plt.xlabel('Date')
+savefig('normalized.pdf',format='pdf')
+
+#
+# Plot daily returns
+#
+plt.clf()
+plt.cla()
+ret.returnize0(normdat)
+plt.plot(newtimestamps[0:50],normdat[0:50,3]) # SPY 50 days
+plt.plot(newtimestamps[0:50],normdat[0:50,4]) # XOM 50 days
+plt.axhline(y=0,color='r')
+plt.legend(['SPY','XOM'])
+plt.ylabel('Daily Returns')
+plt.xlabel('Date')
+savefig('rets.pdf',format='pdf')
+
+#
+# Scatter plat
+#
+plt.clf()
+plt.cla()
+plt.scatter(normdat[:,3],normdat[:,4],c='blue') # SPY v XOM
+plt.ylabel('XOM')
+plt.xlabel('SPY')
+savefig('scatter.pdf',format='pdf')
+
