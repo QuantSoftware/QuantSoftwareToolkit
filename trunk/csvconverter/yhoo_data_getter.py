f4e95ad8861fa8dbec202709effe314e628f8751
sjoshi42 <sjoshi42@gatech.com>
2011-04-10 16:30:55 +0000

    Paths need fixing. Major change- now writes out data oldest timestamp first and newest timestamp last.

--- trunk/csvconverter/yhoo_data_getter.py
+++ trunk/csvconverter/yhoo_data_getter.py
@@ -12,21 +12,44 @@ import datetime
 
 def main ():
     
-    data_access= da.DataAccess('norgate')
-    symbol_list= data_access.get_all_symbols()
-    data_path= "/nethome/sjoshi42/temp/yhoo_trial/"
+    #data_access= da.DataAccess('norgate')
+    #symbol_list= data_access.get_all_symbols()
+    data_path= "C:\\temp\\"
+    symbol_list= list()
+    symbol_list.append("AAPL")
     
     _now =datetime.datetime.now();
     for symbol in symbol_list:
+        symbol_data=list()
         print "Getting " + str (symbol)
         
         try:
             params= urllib.urlencode ({'a':03, 'b':12, 'c':2000, 'd':_now.month, 'e':_now.day, 'f':_now.year, 's': str(symbol)})
             url_get= urllib2.urlopen("http://ichart.finance.yahoo.com/table.csv?%s" % params)
             
+            header= url_get.readline()
+            symbol_data.append (url_get.readline())
+            while (len(symbol_data[-1]) > 0):
+                symbol_data.append(url_get.readline())
+                print str(symbol_data[-1])
+            
+            symbol_data.pop() #The last element is going to be the string of length zero. We don't want to write that to file.
+            #now writing data to file
             f= open (data_path + symbol + ".csv", 'w')
-            f.write (url_get.read())
-            f.close()
+            
+            #Writing the header
+            f.write (header)
+            while (len(symbol_data) > 1):
+                f.write (symbol_data.pop())
+             
+            f.close();    
+               
+               
+                           
+#            print url_get.readline()
+#            f= open (data_path + symbol + ".csv", 'w')
+#            f.write (url_get.read())
+#            f.close()
                         
         except urllib2.HTTPError:
             got_data= False;
