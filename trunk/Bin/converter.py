732c14a01b7cbf560630b94cf76461f65f3949cf
dbratcher <dbratcher@gatech.edu>
2011-04-12 12:58:35 +0000

    bug fixes

--- trunk/Bin/converter.py
+++ trunk/Bin/converter.py
@@ -11,15 +11,17 @@ from pylab import *
 from qstkutil import DataAccess as da
 from qstkutil import timeutil as tu
 from quicksim import quickSim
-from qstkutil import pseries as ps
 from pandas import *
 import matplotlib.pyplot as plt
 import cPickle
 
 def fundsToPNG(funds,output_file):
 	plt.clf()
-	for i in range(0,len(funds)):
-		plt.plot(funds[i].index,funds[i].values)
+	if(type(funds)==type(list())):
+		for i in range(0,len(funds)):
+			plt.plot(funds[i].index,funds[i].values)
+	else:
+		plt.plot(funds.index,funds.values)
 	plt.ylabel('Fund Value')
 	plt.xlabel('Date')
 	plt.draw()
