56e8a55d711e0889c4425d58697f2419dffd53f6
sjoshi42 <sjoshi42@gatech.com>
2011-04-22 22:28:35 +0000

    DataAccess now accesses Yahoo data. Separated CSV to PKL converter into norgate and yahoo converter. Setexample now modified to work with Yahoo data.

--- trunk/csvconverter/yhoo_data_getter.py
+++ trunk/csvconverter/yhoo_data_getter.py
@@ -10,22 +10,34 @@ import urllib2
 import urllib
 import datetime
 import sys
+import os
 
-#from twisted.conch import error
-
-def main ():
+def main():
+    #Getting path
+    path= os.environ['QSDATA']
+    
+    get_data_for_exchange("NASDAQ", path)
+#    get_data_for_exchange("NYSE", path)
+#    get_data_for_exchange("AMEX", path)
+    
+    #main ends
+def get_data_for_exchange (exchange, data_path):
     
     #data_access= da.DataAccess('norgate')
     #symbol_list= data_access.get_all_symbols()
-    data_path= "/nethome/sjoshi42/temp/yhoo_trial/"
+    data_path= data_path + "/Raw/Yahoo/US/" + str (exchange) + "/"
+    
+    #Create path if it doesn't exist
+    if not (os.access(data_path, os.F_OK)):
+        os.makedirs(data_path)
+    
     symbol_list= list()
+    symbol_list.append("MKTG")
+    
     print "Getting list of stocks.."
     
-    #Getting lists of stocks
-    nasdaq_list_str='http://www.nasdaq.com/screening/companies-by-name.aspx?letter=0&exchange=nasdaq&render=download'
-    
     try:
-        nasdaq_params= urllib.urlencode ({'exchange':'nasdaq', 'render':'download'})
+        nasdaq_params= urllib.urlencode ({'exchange':str(exchange), 'render':'download'})
         nasdaq_get= urllib2.urlopen ('http://www.nasdaq.com/screening/companies-by-name.aspx?%s' % nasdaq_params)
         symbol_list.append (nasdaq_get.readline()) #Now we have all the data in a list- but we need only the symbols so we remove the rest
         while (len (symbol_list[-1]) > 0):
@@ -33,18 +45,19 @@ def main ():
             #while ends
         symbol_list.pop(0) #This is just the word "symbol" and not a symbol itself    
         symbol_list.pop(-1) #Remove the last element because its going to be blank anyway    
-        symbol_list = map(lambda x:(x.partition(str(","))[0]),symbol_list) #Get the stuff before the first comma- which is the symbol
+        #symbol_list = map(lambda x:(x.partition(str(","))[0]),symbol_list) #Get the stuff before the first comma- which is the symbol
         
         #Unfortunately this symbol is in quotes. So we have to remove them now
         symbol_list = map(lambda x:(x.partition(str("\""))[2]),symbol_list) #Keep the stuff only after the first "
         symbol_list = map(lambda x:(x.partition(str("\""))[0]),symbol_list) #Keep the stuff before the second "
+        
         
     except urllib2.HTTPError:
         print "Unable to get list of stocks from server. Please check your internet connection and retry."
     except:
         print"Unknown error occoured when getting list of stocks from server."
     
-    print symbol_list
+#    print symbol_list
             
     print "Got " + str (len(symbol_list)) + " symbols. Now getting symbol data..."
     _now =datetime.datetime.now();
@@ -64,19 +77,21 @@ def main ():
                 symbol_data.append(url_get.readline())
 #                print str(symbol_data[-1])
             
-            symbol_data.pop() #The last element is going to be the string of length zero. We don't want to write that to file.
+            symbol_data.pop(-1) #The last element is going to be the string of length zero. We don't want to write that to file.
+            #Following changes so that the data looks like Norgate data and the change to cav_to_pkl.csv is minimized
+            symbol_data = map(lambda x:(x.replace("-", "")),symbol_data) 
+            symbol_data = map(lambda x:(str(symbol) + "," + str(x)) ,symbol_data) #This means that the header is wrong but since it is ignored later anyways- this will work 
+
             #now writing data to file
             f= open (data_path + symbol + ".csv", 'w')
             
             #Writing the header
             f.write (header)
-            while (len(symbol_data) > 1):
+            
+            while (len(symbol_data) > 0):
                 f.write (symbol_data.pop())
              
             f.close();    
-               
-               
-                           
 #            print url_get.readline()
 #            f= open (data_path + symbol + ".csv", 'w')
 #            f.write (url_get.read())
@@ -92,7 +107,7 @@ def main ():
             
         
         #for ends
-    print "All done. Got " + str (len(symbol_list) - miss_ctr) + " stocks. Could not get " + str (miss_ctr) + "stocks."   
+    print "All done. Got " + str (len(symbol_list) - miss_ctr) + " stocks. Could not get " + str (miss_ctr) + " stocks."   
     #main ends
 
 
