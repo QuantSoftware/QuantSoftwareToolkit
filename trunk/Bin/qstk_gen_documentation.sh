01a36aa9663b963df3e0ddd5bb4d75ea07f1dc5d
sjoshi42 <sjoshi42@gatech.com>
2011-05-16 17:07:58 +0000

    Updated script to remove olds docs and add new docs to svn. Note: script does not automatically commit.

--- trunk/Bin/qstk_gen_documentation.sh
+++ trunk/Bin/qstk_gen_documentation.sh
@@ -5,6 +5,15 @@ OLD_PY_PATH=$PYTHONPATH
 export PYTHONPATH=":$QS/${EPYDOC_PATH}"$PYTHONPATH
 rm ${HTML_DOCS_PATH}/*
 rm ${PDF_DOCS_PATH}/*
+svn del --force ${HTML_DOCS_PATH}
+svn del --force ${PDF_DOCS_PATH}
+
+mkdir ${HTML_DOCS_PATH}
+mkdir ${PDF_DOCS_PATH}
+
 $QS/${EPYDOC_PATH}/scripts/epydoc --html $QS -o ${HTML_DOCS_PATH} --name QSTK
 $QS/${EPYDOC_PATH}/scripts/epydoc --pdf $QS -o ${PDF_DOCS_PATH} --name QSTK
 export PYTHONPATH=${OLD_PY_PATH}
+svn add ${HTML_DOCS_PATH}
+svn add ${PDF_DOCS_PATH}
+echo "NOTE: New documentation is still not committed to repository. Please use svn commit do commit to repository."
