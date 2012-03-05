8df4264dba2d3ce463164f440b7d4f43a0c841bd
Sourabh Bajaj <sb2nov@gmail.com>
2012-03-05 20:12:20 +0000

    Event Profiler Tutorial

---
+++ trunk/Examples/EventProfiler/tutorial.py
@@ -0,0 +1,108 @@
+'''
+(c) 2011, 2012 Georgia Tech Research Corporation
+This source code is released under the New BSD license.  Please see
+http://wiki.quantsoftware.org/index.php?title=QSTK_License
+for license details.
+
+Created on March, 5, 2012
+
+@author: Sourabh Bajaj
+@contact: sourabhbajaj90@gmail.com
+@summary: Event Profiler Tutorial
+'''
+
+
+import pandas 
+from qstkutil import DataAccess as da
+import numpy as np
+import math
+import qstkutil.dateutil as du
+import datetime as dt
+import qstkutil.DataAccess as da
+import qstkutil.tsutil as tsu
+import EventProfiler as ep
+
+"""
+Accepts a list of symbols along with start and end date
+Returns the Event Matrix which is a pandas Datamatrix
+Event matrix has the following structure :
+    |IBM |GOOG|XOM |MSFT| GS | JP |
+(d1)|nan |nan | 1  |nan |nan | 1  |
+(d2)|nan | 1  |nan |nan |nan |nan |
+(d3)| 1  |nan | 1  |nan | 1  |nan |
+(d4)|nan |  1 |nan | 1  |nan |nan |
+...................................
+...................................
+Also, d1 = start date
+nan = no information about any event.
+1 = status bit(positively confirms the event occurence)
+"""
+
+# Get the data from the data store
+storename = "Norgate" # get data from our daily prices source
+# Available field names: open, close, high, low, close, actual_close, volume
+closefield = "close"
+volumefield = "volume"
+window = 10
+
+def findEvents(symbols, startday,endday, marketSymbol,verbose=False):
+
+	# Reading the Data for the list of Symbols.	
+	timeofday=dt.timedelta(hours=16)
+	timestamps = du.getNYSEdays(startday,endday,timeofday)
+	dataobj = da.DataAccess('Norgate')
+	if verbose:
+            print __name__ + " reading data"
+	# Reading the Data
+	close = dataobj.get_data(timestamps, symbols, closefield)
+	
+	# Completing the Data - Removing the NaN values from the Matrix
+	close = (close.fillna()).fillna(method='backfill')
+	close = (close.fillna()).fillna(method='ffill')
+	
+	# Calculating Daily Returns for the Market
+	tsu.returnize0(close.values)
+	SPYValues=close[marketSymbol]
+
+	# Calculating the Returns of the Stock Relative to the Market 
+	# So if a Stock went up 5% and the Market rised 3%. The the return relative to market is 2% 
+	mktneutDM = close - close[marketSymbol] 
+
+	if verbose:
+            print __name__ + " finding events"
+
+	# Generating the Event Matrix
+	# Event described is : Market falls more than 3% plus the stock falls 3% more than the Market
+	# Suppose : The market fell 3%, then the stock should fall more than 8% to mark the event.
+	# And if the market falls 5%, then the stock should fall more than 10% to mark the event.
+
+	for symbol in symbols:
+		# If the Symbol did not fall more than 5% compared to the Market.
+	    mktneutDM[symbol][mktneutDM[symbol]> -0.05] = np.NAN
+		
+	    for i in range(1,len(mktneutDM[symbol])):
+	        if SPYValues[i]<-0.03 and mktneutDM[symbol][i] < -0.05 : # When market fall is more than 3% and also the stock compared to market is also fell by more than 5%.
+             		mktneutDM[symbol][i] = 1.0  #overwriting the price by the bit
+			
+	    mktneutDM[symbol][mktneutDM[symbol]< 1.0] = np.NAN  #Filling rest of the places as NaN
+	return mktneutDM
+
+
+#################################################
+################ MAIN CODE ######################
+#################################################
+
+
+symbols = np.loadtxt('SP500port.csv',dtype='S10',comments='#', skiprows=1)
+# You might get a message about some files being missing, don't worry about it.
+
+#symbols =['BFRE','ATCS','RSERF','GDNEF','LAST','ATTUF','JBFCF','CYVA','SPF','XPO','EHECF','TEMO','AOLS','CSNT','REMI','GLRP','AIFLY','BEE','DJRT','CHSTF','AICAF']
+startday = dt.datetime(2008,1,1)
+endday = dt.datetime(2010,12,31)
+eventMatrix = findEvents(symbols,startday,endday,marketSymbol='SPY',verbose=True)
+
+eventProfiler = ep.EventProfiler(eventMatrix,startday,endday,lookback_days=20,lookforward_days=20,verbose=True)
+
+eventProfiler.study(filename="MyEventStudy.pdf",plotErrorBars=True,plotMarketNeutral=True,plotEvents=False,marketSymbol='SPY')
+
+
