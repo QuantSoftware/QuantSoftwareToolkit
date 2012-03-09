0639c5d40629529c78c66a6404f2f80dcdb9b0f2
Sourabh Bajaj <sb2nov@gmail.com>
2012-03-09 01:43:34 +0000

    Visualizer Modified

--- trunk/Tools/Visualizer/Visualizer.py
+++ trunk/Tools/Visualizer/Visualizer.py
@@ -1,4 +1,6 @@
 import sys
+import numpy as np
+import AccessData as AD
 from PyQt4 import QtGui, QtCore, Qt
 from matplotlib.figure import Figure
 import matplotlib.pyplot as plt
@@ -178,10 +180,46 @@ class RangeSlider(QtGui.QSlider):
                                              opt.upsideDown)
 
 
+###########################
+##  Visualizer Class     ##
+###########################
+
+
 class Visualizer(QtGui.QMainWindow):
     
-	def __init__(self):
+	def __init__(self, PandasObject1, featureslist1, symbols1, timestamps1, dMinFeat1, dMaxFeat1, startday1, endday1):
 		super(Visualizer, self).__init__()
+
+		self.PandasObject=PandasObject1
+		self.featureslist=featureslist1
+		self.symbols=symbols1
+		self.timestamps=timestamps1
+		self.dMinFeat=dMinFeat1
+		self.dMaxFeat=dMaxFeat1
+		self.startday=startday1
+		self.endday=endday1
+		self.Xfeature=self.featureslist[0]
+		self.Yfeature=self.featureslist[0]
+		self.Zfeature=self.featureslist[0]
+		self.Sfeature=self.featureslist[0]
+		self.Cfeature=self.featureslist[0]
+		self.XMin=0.0
+		self.XMax=1.0
+		self.XLow=self.XMin
+		self.XHigh=self.XMax
+		self.YMin=0.0
+		self.YMax=1.0
+		self.YLow=self.YMin
+		self.YHigh=self.YMax
+		self.ZMin=0.0
+		self.ZMax=1.0
+		self.ZLow=self.ZMin
+		self.ZHigh=self.ZMax
+		self.SMin=0.0
+		self.SMax=1.0
+		self.CMin=0.0
+		self.CMax=1.0
+		self.dayofplot=self.timestamps[0]
 		self.create_main_frame()
         
 	def create_main_frame(self):
@@ -194,8 +232,8 @@ class Visualizer(QtGui.QMainWindow):
 		
 		self.canvas = FigureCanvas(self.fig)
 		self.canvas.setParent(self.main_frame)
-		self.ax = self.fig.gca(projection='3d')
 
+		self.ax = self.fig.gca(projection='3d')
 
 #		self.mpl_toolbar = NavigationToolbar(self.canvas, self.main_frame)
 		
@@ -205,7 +243,7 @@ class Visualizer(QtGui.QMainWindow):
 		self.font2 = QtGui.QFont("Times", 14, QtGui.QFont.Bold, True)
 		self.font3 = QtGui.QFont("Times", 20, QtGui.QFont.Bold, True)
 		
-		self.VisLable = QtGui.QLabel('DataVisualizer', self)
+		self.VisLable = QtGui.QLabel('qstkViz', self)
 		
 		self.VisLable.setFont(self.font3)
 		self.FactorLable.setFont(self.font)
@@ -215,20 +253,22 @@ class Visualizer(QtGui.QMainWindow):
 
 		self.XCombo = QtGui.QComboBox(self)
 		self.FeatureComboBox(self.XCombo)	
-		self.XCombo.activated[str].connect(self.ComboActivated)
+		self.XCombo.activated[str].connect(self.XComboActivated)
 
-		self.XSpace=QtGui.QLabel('    ', self)
+		self.XSpace=QtGui.QLabel(' ', self)
 
-		self.XMinLcd = QtGui.QLCDNumber(3, self)
-		self.XMaxLcd = QtGui.QLCDNumber(3, self)
+		self.XMinLcd = QtGui.QLCDNumber(4, self)
+		self.XMaxLcd = QtGui.QLCDNumber(4, self)
+		self.XMinLcd.setSegmentStyle(2)
+		self.XMaxLcd.setSegmentStyle(2)
 
-		self.XMinLable=QtGui.QLabel('Min', self)
+		self.XMinLable=QtGui.QLabel(str(self.XMin), self)
 		self.XMinLable.setFont(self.font1)
 		
 		self.XRange=RangeSlider(Qt.Qt.Horizontal)
-		self.InitRangeSlider(self.XRange)
-		
-		self.XMaxLable=QtGui.QLabel('Max', self)
+		self.XInitRangeSlider(self.XRange)
+	
+		self.XMaxLable=QtGui.QLabel(str(self.XMax), self)
 		self.XMaxLable.setFont(self.font1)
 
 		self.YLable=QtGui.QLabel('Y', self)
@@ -236,20 +276,22 @@ class Visualizer(QtGui.QMainWindow):
 
 		self.YCombo = QtGui.QComboBox(self)
 		self.FeatureComboBox(self.YCombo)	
-		self.YCombo.activated[str].connect(self.ComboActivated)
+		self.YCombo.activated[str].connect(self.YComboActivated)
 
-		self.YSpace=QtGui.QLabel('    ', self)
+		self.YSpace=QtGui.QLabel(' ', self)
 
-		self.YMinLcd = QtGui.QLCDNumber(3, self)
-		self.YMaxLcd = QtGui.QLCDNumber(3, self)
+		self.YMinLcd = QtGui.QLCDNumber(4, self)
+		self.YMaxLcd = QtGui.QLCDNumber(4, self)
+		self.YMinLcd.setSegmentStyle(2)
+		self.YMaxLcd.setSegmentStyle(2)
 
-		self.YMinLable=QtGui.QLabel('Min', self)
+		self.YMinLable=QtGui.QLabel(str(self.YMin), self)
 		self.YMinLable.setFont(self.font1)
 		
 		self.YRange=RangeSlider(Qt.Qt.Horizontal)
-		self.InitRangeSlider(self.YRange)
+		self.YInitRangeSlider(self.YRange)
 
-		self.YMaxLable=QtGui.QLabel('Max', self)
+		self.YMaxLable=QtGui.QLabel(str(self.YMax), self)
 		self.YMaxLable.setFont(self.font1)
 
 		self.ZLable=QtGui.QLabel('Z', self)
@@ -257,20 +299,22 @@ class Visualizer(QtGui.QMainWindow):
 	
 		self.ZCombo = QtGui.QComboBox(self)
 		self.FeatureComboBox(self.ZCombo)	
-		self.ZCombo.activated[str].connect(self.ComboActivated)
+		self.ZCombo.activated[str].connect(self.ZComboActivated)
 	
-		self.ZSpace=QtGui.QLabel('    ', self)
+		self.ZSpace=QtGui.QLabel(' ', self)
 		
-		self.ZMinLcd = QtGui.QLCDNumber(3, self)
-		self.ZMaxLcd = QtGui.QLCDNumber(3, self)
+		self.ZMinLcd = QtGui.QLCDNumber(4, self)
+		self.ZMaxLcd = QtGui.QLCDNumber(4, self)
+		self.ZMinLcd.setSegmentStyle(2)
+		self.ZMaxLcd.setSegmentStyle(2)
 
-		self.ZMinLable=QtGui.QLabel('Min', self)
+		self.ZMinLable=QtGui.QLabel(str(self.ZMin), self)
 		self.ZMinLable.setFont(self.font1)
 		
 		self.ZRange=RangeSlider(Qt.Qt.Horizontal)
-		self.InitRangeSlider(self.ZRange)
+		self.ZInitRangeSlider(self.ZRange)
 
-		self.ZMaxLable=QtGui.QLabel('Max', self)
+		self.ZMaxLable=QtGui.QLabel(str(self.ZMax), self)
 		self.ZMaxLable.setFont(self.font1)
 
 		self.Frame1= QtGui.QFrame()
@@ -279,16 +323,17 @@ class Visualizer(QtGui.QMainWindow):
 		self.DLable=QtGui.QLabel('Date ', self)
 		self.DLable.setFont(self.font2)
 
-		self.DateLable=QtGui.QLabel('1-1-2011', self)
+		self.DateLable=QtGui.QLabel(self.startday.date().isoformat(), self)
 		self.DateLable.setFont(self.font1)
 
-		self.DateMinLable=QtGui.QLabel('Start Date', self)
+		self.DateMinLable=QtGui.QLabel(self.startday.date().isoformat(), self)
 		self.DateMinLable.setFont(self.font1)
 
 		self.DateSlider= QtGui.QSlider(QtCore.Qt.Horizontal, self)
+		self.DateSlider.setRange(0,len(self.timestamps)-1)
 		self.DateSlider.valueChanged.connect(self.DateActivated)
 
-		self.DateMaxLable=QtGui.QLabel('End Date', self)
+		self.DateMaxLable=QtGui.QLabel(self.endday.date().isoformat(), self)
 		self.DateMaxLable.setFont(self.font1)
 
 		self.Frame2= QtGui.QFrame()
@@ -300,19 +345,24 @@ class Visualizer(QtGui.QMainWindow):
 		self.Frame4= QtGui.QFrame()
 		self.Frame4.setFrameShape(4)
 
-		self.SizeLable=QtGui.QLabel('Size', self)
+		self.SizeLable=QtGui.QLabel('Size   ', self)
 		self.SizeLable.setFont(self.font2)
 
 		self.SizeCombo = QtGui.QComboBox(self)
 		self.FeatureComboBox(self.SizeCombo)	
-		self.SizeCombo.activated[str].connect(self.ComboActivated)
+		self.SizeCombo.activated[str].connect(self.SComboActivated)
 
 		self.ColorLable=QtGui.QLabel('Color', self)
 		self.ColorLable.setFont(self.font2)
 
 		self.ColorCombo = QtGui.QComboBox(self)
 		self.FeatureComboBox(self.ColorCombo)	
-		self.ColorCombo.activated[str].connect(self.ComboActivated)
+		self.ColorCombo.activated[str].connect(self.CComboActivated)
+
+		self.UpdateButton =QtGui.QPushButton('Plot',self)
+		self.UpdateButton.setToolTip('Update the plot')
+		self.UpdateButton.resize(self.UpdateButton.sizeHint())
+		self.UpdateButton.clicked.connect(self.PlotCanvas)
 
 		ExitAction = QtGui.QAction(QtGui.QIcon('Exit.png'), 'Exit', self)
 		ExitAction.setShortcut('Ctrl+Q')
@@ -382,11 +432,31 @@ class Visualizer(QtGui.QMainWindow):
 			Zhbox2.addWidget(w)
 			Zhbox2.setAlignment(w, QtCore.Qt.AlignVCenter)
 
-		schbox = QtGui.QHBoxLayout()
+		shbox = QtGui.QHBoxLayout()
 
-		for w in [  self.SizeLable, self.SizeCombo, self.ColorLable, self.ColorCombo]:
-			schbox.addWidget(w)
-			schbox.setAlignment(w, QtCore.Qt.AlignVCenter)
+		for w in [  self.SizeLable, self.SizeCombo]:
+			shbox.addWidget(w)
+			shbox.setAlignment(w, QtCore.Qt.AlignVCenter)
+		
+		shbox.addStretch(1)
+
+		chbox = QtGui.QHBoxLayout()
+
+		for w in [self.ColorLable, self.ColorCombo]:
+			chbox.addWidget(w)
+			chbox.setAlignment(w, QtCore.Qt.AlignVCenter)
+
+		chbox.addStretch(1)
+		
+		scvbox = QtGui.QVBoxLayout()
+		scvbox.addLayout(shbox)
+		scvbox.addLayout(chbox)
+
+		schbox = QtGui.QHBoxLayout()
+		schbox.addLayout(scvbox)
+		schbox.setAlignment(schbox, QtCore.Qt.AlignVCenter)
+		schbox.addWidget(self.UpdateButton)
+		schbox.setAlignment(self.UpdateButton, QtCore.Qt.AlignVCenter)
 
 		Datehbox1 = QtGui.QHBoxLayout()
 		Datehbox1.addWidget(self.DLable)
@@ -413,6 +483,8 @@ class Visualizer(QtGui.QMainWindow):
 		Vbox1.addLayout(Datehbox2)
 		Vbox1.addWidget(self.Frame2)
 		Vbox1.addLayout(schbox)
+#		Vbox1.addLayout(shbox)
+#		Vbox1.addLayout(chbox)
 		Vbox1.addWidget(self.Frame4)
 
 		Finalvbox= QtGui.QVBoxLayout()
@@ -422,48 +494,150 @@ class Visualizer(QtGui.QMainWindow):
 		FinalBox= QtGui.QHBoxLayout()
 		FinalBox.addLayout(Finalvbox)
 		FinalBox.addLayout(Vbox1)
+		FinalBox.addStretch(1)
 		
 		SuperFinalBox = QtGui.QVBoxLayout()
 		SuperFinalBox.addWidget(self.VisLable)
 		SuperFinalBox.addLayout(FinalBox)
+		SuperFinalBox.addStretch(1)
 
-		self.setWindowTitle('DataVisualizer')
+		self.setWindowTitle('qstkViz')
 		self.setWindowIcon(QtGui.QIcon('V.png'))
 
 		self.main_frame.setLayout(SuperFinalBox)
 		self.setCentralWidget(self.main_frame)        
 
 	def FeatureComboBox(self, combo):
-		combo.addItem("Ubuntu")
-		combo.addItem("Mandriva")
-		combo.addItem("Fedora")
-		combo.addItem("Red Hat")
-		combo.addItem("Gentoo")
+		for feat in self.featureslist:
+			combo.addItem(feat)
         
-	def ComboActivated(self,text):
-		self.statusBar().showMessage(text)
+	def XComboActivated(self,text):
+		self.Xfeature=text
+		self.XMax=self.dMaxFeat[str(self.Xfeature)]
+		self.XMin=self.dMinFeat[str(self.Xfeature)]
+		self.XMinLable.setText(str(round(self.XMin,1)))
+		self.XMaxLable.setText(str(round(self.XMax,1)))
+		self.XChangeValues(self.XRange.low(), self.XRange.high())
 
-	def ChangeValues(self, low, high):
-		print 'Low is '+ str(low) + ' High is ' + str(high)
+	def YComboActivated(self,text):
+		self.Yfeature=text
+		self.YMax=self.dMaxFeat[str(self.Yfeature)]
+		self.YMin=self.dMinFeat[str(self.Yfeature)]
+		self.YMinLable.setText(str(round(self.YMin,1)))
+		self.YMaxLable.setText(str(round(self.YMax,1)))
+		self.YChangeValues(self.YRange.low(), self.YRange.high())
 
-	def InitRangeSlider(self, slider):
+	def ZComboActivated(self,text):
+		self.Zfeature=text
+		self.ZMax=self.dMaxFeat[str(self.Zfeature)]
+		self.ZMin=self.dMinFeat[str(self.Zfeature)]
+		self.ZMinLable.setText(str(round(self.ZMin,1)))
+		self.ZMaxLable.setText(str(round(self.ZMax,1)))
+		self.ZChangeValues(self.ZRange.low(), self.ZRange.high())
+
+	def SComboActivated(self,text):
+		self.Sfeature=text
+		self.SMax=self.dMaxFeat[str(self.Sfeature)]
+		self.SMin=self.dMinFeat[str(self.Sfeature)]
+
+	def CComboActivated(self,text):
+		self.Cfeature=text
+		self.CMax=self.dMaxFeat[str(self.Cfeature)]
+		self.CMin=self.dMinFeat[str(self.Cfeature)]
+
+	def XChangeValues(self, low, high):
+		self.XLow=low*0.02*(self.XMax-self.XMin)+self.XMin
+		self.XHigh=high*0.02*(self.XMax-self.XMin)+self.XMin		
+		self.XMinLcd.display(self.XLow)
+		self.XMaxLcd.display(self.XHigh)		
+
+	def XInitRangeSlider(self, slider):
 		slider.setMinimum(0)
-		slider.setMaximum(10000)
+		slider.setMaximum(50)
 		slider.setLow(0)
-		slider.setHigh(10000)
-		slider.setTickPosition(QtGui.QSlider.TicksBelow)
-		QtCore.QObject.connect(slider, QtCore.SIGNAL('sliderMoved'), self.ChangeValues)
+		slider.setHigh(50)
+		QtCore.QObject.connect(slider, QtCore.SIGNAL('sliderMoved'), self.XChangeValues)
 
-	def DateActivated(self, text):
-		self.DateLable.setText(str(text))
+	def YChangeValues(self, low, high):
+		self.YLow=low*0.02*(self.YMax-self.YMin)+self.YMin
+		self.YHigh=high*0.02*(self.YMax-self.YMin)+self.YMin		
+		self.YMinLcd.display(self.YLow)
+		self.YMaxLcd.display(self.YHigh)	
+
+	def YInitRangeSlider(self, slider):
+		slider.setMinimum(0)
+		slider.setMaximum(50)
+		slider.setLow(0)
+		slider.setHigh(50)
+		QtCore.QObject.connect(slider, QtCore.SIGNAL('sliderMoved'), self.YChangeValues)
+
+	def ZChangeValues(self, low, high):
+		self.ZLow=low*0.02*(self.ZMax-self.ZMin)+self.ZMin
+		self.ZHigh=high*0.02*(self.ZMax-self.ZMin)+self.ZMin		
+		self.ZMinLcd.display(self.ZLow)
+		self.ZMaxLcd.display(self.ZHigh)		
+
+	def ZInitRangeSlider(self, slider):
+		slider.setMinimum(0)
+		slider.setMaximum(50)
+		slider.setLow(0)
+		slider.setHigh(50)
+		QtCore.QObject.connect(slider, QtCore.SIGNAL('sliderMoved'), self.ZChangeValues)
+
+	def DateActivated(self, index):
+		self.dayofplot=self.timestamps[index]
+		dateval=self.timestamps[index]
+		dateval=dateval.date().isoformat()
+		self.DateLable.setText(dateval)
 		self.DateLable.adjustSize() 
 
+	def inrange(self, x, minx, maxx):
+		if x>minx and x<maxx: return x
+		else: return np.NAN
+
+	def scale(self, x, minx, maxx):
+		return (599*((x-minx)/(maxx-minx))+10)
+
+	def PlotCanvas(self):
+		self.ax.cla()
+		self.ax.mouse_init(rotate_btn=1, zoom_btn=3)
+		self.ax.set_frame_on(False)
+
+		xs=self.PandasObject[str(self.Xfeature)].xs(self.dayofplot)
+		ys=self.PandasObject[str(self.Yfeature)].xs(self.dayofplot)
+		zs=self.PandasObject[str(self.Zfeature)].xs(self.dayofplot)
+	
+		size=self.PandasObject[str(self.Sfeature)].xs(self.dayofplot)
+		color=self.PandasObject[str(self.Cfeature)].xs(self.dayofplot)
+
+		xs1 = [self.inrange(x, self.XLow, self.XHigh) for x in xs]
+		ys1 = [self.inrange(y, self.YLow, self.YHigh) for y in ys]
+		zs1 = [self.inrange(z, self.ZLow, self.ZHigh) for z in zs]
+
+		size1 =  [self.scale(s, self.SMin, self.SMax) for s in size]
+		color1 = [self.scale(c, self.CMin, self.CMax) for c in color]
+
+		p=self.ax.scatter(xs1,ys1,zs1,marker='o', alpha=0.5, c=color1, s=size1)
+		
+		self.ax.set_xlim(self.XLow, self.XHigh)
+		self.ax.set_ylim(self.YLow, self.YHigh)
+		self.ax.set_zlim(self.ZLow, self.ZHigh)	
+	
+		self.ax.set_xlabel(self.Xfeature)
+		self.ax.set_ylabel(self.Yfeature)
+		self.ax.set_zlabel(self.Zfeature)
+
+		self.canvas.draw()
+		self.statusBar().showMessage('Update the Plot')
+
 def main():
-    
-    app = QtGui.QApplication(sys.argv)
-    ex = Visualizer()
-    ex.show()
-    sys.exit(app.exec_())
+	(PandasObject, featureslist, symbols, timestamps)=AD.ReadData()
+	(dMinFeat, dMaxFeat, startday, endday)=AD.DataParameter(PandasObject, featureslist, symbols, timestamps)
+	
+	app = QtGui.QApplication(sys.argv)
+	ex = Visualizer(PandasObject, featureslist, symbols, timestamps, dMinFeat, dMaxFeat, startday, endday)
+	ex.show()
+	sys.exit(app.exec_())
 
 
 if __name__ == '__main__':
