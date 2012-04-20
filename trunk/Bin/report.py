28c0e9b00d9061c27725a1a7ffb0f58740fef5f1
dbratcher <dbratcher@gatech.edu>
2012-04-20 17:41:44 +0000

    edited code to conform to pylint

--- trunk/Bin/report.py
+++ trunk/Bin/report.py
@@ -1,18 +1,21 @@
-#
-# report.py
-#
-# Generates a html file containing a report based 
-# off a timeseries of funds from a pickle file.
-#
-# Drew Bratcher
-#
+'''
+(c) 2011, 2012 Lucena Research, LLC.  All rights reserved.
+
+Created on 4/4/2011
+
+@author: Drew Bratcher
+@contact: dbratcher@gatech.edu
+@summary:  Generates a html file containing a report based 
+           off a timeseries of funds from a pickle file.
+'''
+
 from os import path
+from os import sys
 from pylab import *
 from qstkutil import DataAccess as da
 from qstkutil import dateutil as du
 from qstkutil import tsutil as tsu
 from qstkutil import fundutil as fu
-from quicksim import quickSim as qs
 import converter
 
 from pandas import *
@@ -20,296 +23,266 @@ import matplotlib.pyplot as plt
 import cPickle
 import datetime as dt
 
-BENCHMARK_CLOSE=0
+BENCHMARK_CLOSE = 0
 
 def print_header(html_file, name):
-	#top
-	html_file.write("<HTML>\n")
-	html_file.write("<HEAD>\n")
-	html_file.write("<TITLE>QSTK Generated Report:"+name+"</TITLE>\n")
-	html_file.write("</HEAD>\n\n")
-	html_file.write("<BODY>\n\n")
+    """
+    @summary prints header of report html file
+    """
+    html_file.write("<HTML>\n")
+    html_file.write("<HEAD>\n")
+    html_file.write("<TITLE>QSTK Generated Report:" + name + "</TITLE>\n")
+    html_file.write("</HEAD>\n\n")
+    html_file.write("<BODY>\n\n")
 
 def print_footer(html_file):
-	html_file.write("</BODY>\n\n")
-	html_file.write("</HTML>")
+    """
+    @summary prints footer of report html file
+    """
+    html_file.write("</BODY>\n\n")
+    html_file.write("</HTML>")
 
-def print_stats(funds, benchmark, name, outstream=sys.stdout):
-	start_date=funds.index[0].strftime("%m/%d/%Y")
-	end_date=funds.index[-1].strftime("%m/%d/%Y")
-	outstream.write("Performance Summary for "+str(path.basename(name))+" Backtest (Long Only)\n")
-	outstream.write("For the dates "+str(start_date)+" to "+str(end_date)+"\n\n")
-	outstream.write("Yearly Performance Metrics \n")
-	years=du.getYears(funds)
-	outstream.write("\n\t\t\t\t")
-	for year in years:
-		outstream.write("\t\t"+str(year))
-	outstream.write("\n")
-	outstream.write("Fund Annualized Return:\t\t")
-	for year in years:
-		year_vals=[]
-		for date in funds.index:
-			if(date.year==year):
-				year_vals.append(funds[date])
-		day_rets=tsu.daily1(year_vals)
-		ret=tsu.getRorAnnual(day_rets[1:-1])
-		x="\t\t%+6.2f%%" % (ret*100)
-		outstream.write(x)
+def print_stats(fund_ts, benchmark, name, outstream = sys.stdout):
+    """
+    @summary prints stats of a provided fund and benchmark
+    @param fund_ts: fund value in pandas timeseries
+    @param benchmark: benchmark symbol to compare fund to
+    @param name: name to associate with the fund in the report
+    @param outstream: stream to print stats to, defaults to stdout
+    """
+    start_date = fund_ts.index[0].strftime("%m/%d/%Y")
+    end_date = fund_ts.index[-1].strftime("%m/%d/%Y")
+    outstream.write("Performance Summary for "\
+	 + str(path.basename(name)) + " Backtest (Long Only)\n")
+    outstream.write("For the dates " + str(start_date) + " to "\
+                                       + str(end_date) + "\n\n")
+    outstream.write("Yearly Performance Metrics \n")
+    years = du.getYears(fund_ts)
+    outstream.write("\n\t\t\t\t")
+    for year in years:
+        outstream.write("\t\t" + str(year))
+    outstream.write("\n")
+    outstream.write("Fund Annualized Return:\t\t")
+    for year in years:
+        year_vals = []
+        for date in fund_ts.index:
+            if(date.year ==year):
+                year_vals.append(fund_ts[date])
+        day_rets = tsu.daily1(year_vals)
+        ret = tsu.get_ror_annual(day_rets[1:-1])
+        outstream.write("\t\t% + 6.2f%%" % (ret*100))
 
-	outstream.write("\n"+str(benchmark[0])+" Annualized Return:\t\t")
-	timeofday=dt.timedelta(hours=16)
-	timestamps=du.getNYSEdays(funds.index[0], funds.index[-1], timeofday)
-	dataobj=da.DataAccess('Norgate')
-	global BENCHMARK_CLOSE
-	BENCHMARK_CLOSE=dataobj.get_data(timestamps,benchmark, "close", verbose=False)
-	for year in years:
-		year_vals=[]
-		for date in BENCHMARK_CLOSE.index:
-			if(date.year==year):
-				year_vals.append(BENCHMARK_CLOSE.xs(date))
-		day_rets=tsu.daily1(year_vals)
-		ret=tsu.getRorAnnual(day_rets[1:-1])
-		x="\t\t%+6.2f%%" % (ret*100)
-		outstream.write(x)
+    outstream.write("\n" + str(benchmark[0]) + " Annualized Return:\t\t")
+    timeofday = dt.timedelta(hours = 16)
+    timestamps = du.getNYSEdays(fund_ts.index[0], fund_ts.index[-1], timeofday)
+    dataobj = da.DataAccess('Norgate')
+    global BENCHMARK_CLOSE
+    BENCHMARK_CLOSE = dataobj.get_data(timestamps, benchmark, "close", \
+                                                     verbose = False)
+    for year in years:
+        year_vals = []
+        for date in BENCHMARK_CLOSE.index:
+            if(date.year ==year):
+                year_vals.append(BENCHMARK_CLOSE.xs(date))
+        day_rets = tsu.daily1(year_vals)
+        ret = tsu.get_ror_annual(day_rets[1:-1])
+        outstream.write("\t\t% + 6.2f%%" % (ret*100))
 
-	outstream.write("\n\nFund Winning Days:\t\t")                
-	for year in years:
-		year_vals=[]
-		for date in funds.index:
-			if(date.year==year):
-				year_vals.append(funds[date])
-		ret=fu.getWinningDays(year_vals)
-		x="\t\t%+6.2f%%" % ret
-		outstream.write(x)
+    outstream.write("\n\nFund Winning Days:\t\t")                
+    for year in years:
+        year_vals = []
+        for date in fund_ts.index:
+            if(date.year==year):
+                year_vals.append(fund_ts[date])
+        ret = fu.get_winning_days(year_vals)
+        outstream.write("\t\t% + 6.2f%%" % ret)
 
-	outstream.write("\n"+str(benchmark[0])+" Winning Days:\t\t")                
-	for year in years:
-		year_vals=[]
-		for date in BENCHMARK_CLOSE.index:
-			if(date.year==year):
-				year_vals.append(BENCHMARK_CLOSE.xs(date))
-		ret=fu.getWinningDays(year_vals)
-		x="\t\t%+6.2f%%" % ret
-		outstream.write(x)
+    outstream.write("\n" + str(benchmark[0]) + \
+                       " Winning Days:\t\t")                
+    for year in years:
+        year_vals = []
+        for date in BENCHMARK_CLOSE.index:
+            if(date.year==year):
+                year_vals.append(BENCHMARK_CLOSE.xs(date))
+        ret = fu.get_winning_days(year_vals)
+        outstream.write("\t\t% + 6.2f%%" % ret)
 
-	outstream.write("\n\nFund Max Draw Down:\t\t")
-	for year in years:
-		year_vals=[]
-		for date in funds.index:
-			if(date.year==year):
-				year_vals.append(funds[date])
-		ret=fu.getMaxDrawDown(year_vals)
-		x="\t\t%+6.2f%%" % (ret*100)
-		outstream.write(x)
+    outstream.write("\n\nFund Max Draw Down:\t\t")
+    for year in years:
+        year_vals = []
+        for date in fund_ts.index:
+            if(date.year==year):
+                year_vals.append(fund_ts[date])
+        ret = fu.get_max_draw_down(year_vals)
+        outstream.write("\t\t% + 6.2f%%" % (ret*100))
 
-	outstream.write("\n"+str(benchmark[0])+" Max Draw Down:\t\t")
-	for year in years:
-		year_vals=[]
-		for date in BENCHMARK_CLOSE.index:
-			if(date.year==year):
-				year_vals.append(BENCHMARK_CLOSE.xs(date))
-		ret=fu.getMaxDrawDown(year_vals)
-		x="\t\t%+6.2f%%" % (ret*100)
-		outstream.write(x)
+    outstream.write("\n" + str(benchmark[0]) + " Max Draw Down:\t\t")
+    for year in years:
+        year_vals = []
+        for date in BENCHMARK_CLOSE.index:
+            if(date.year==year):
+                year_vals.append(BENCHMARK_CLOSE.xs(date))
+        ret = fu.get_max_draw_down(year_vals)
+        outstream.write("\t\t% + 6.2f%%" % (ret*100))
 
-	outstream.write("\n\nFund Daily Sharpe Ratio(for year):")
-	for year in years:
-		year_vals=[]
-		for date in funds.index:
-			if(date.year==year):
-				year_vals.append(funds[date])
-		ret=fu.getSharpeRatio(year_vals)
-		x="\t\t%+6.2f" % ret
-		outstream.write(x)
+    outstream.write("\n\nFund Daily Sharpe Ratio(for year):")
+    for year in years:
+        year_vals = []
+        for date in fund_ts.index:
+            if(date.year==year):
+                year_vals.append(fund_ts[date])
+        ret = fu.get_sharpe_ratio(year_vals)
+        outstream.write("\t\t% + 6.2f" % ret)
 
-	outstream.write("\n"+str(benchmark[0])+" Daily Sharpe Ratio(for year):")         
-	for year in years:
-		year_vals=[]
-		for date in BENCHMARK_CLOSE.index:
-			if(date.year==year):
-				year_vals.append(BENCHMARK_CLOSE.xs(date))
-		ret=fu.getSharpeRatio(year_vals)
-		x="\t\t%+6.2f" % ret
-		outstream.write(x)
+    outstream.write("\n" + str(benchmark[0]) + \
+            " Daily Sharpe Ratio(for year):")       
+    for year in years:
+        year_vals = []
+        for date in BENCHMARK_CLOSE.index:
+            if(date.year==year):
+                year_vals.append(BENCHMARK_CLOSE.xs(date))
+        ret = fu.get_sharpe_ratio(year_vals)
+        outstream.write("\t\t% + 6.2f" % ret)
 
-	outstream.write("\n\nFund Daily Sortino Ratio(for year):")
-	for year in years:
-		year_vals=[]
-		for date in funds.index:
-			if(date.year==year):
-				year_vals.append(funds[date])
-		ret=fu.getSortinoRatio(year_vals)
-		x="\t\t%+6.2f" % ret
-		outstream.write(x)
+    outstream.write("\n\nFund Daily Sortino Ratio(for year):")
+    for year in years:
+        year_vals = []
+        for date in fund_ts.index:
+            if(date.year==year):
+                year_vals.append(fund_ts[date])
+        ret = fu.get_sortino_ratio(year_vals)
+        outstream.write("\t\t% + 6.2f" % ret)
 
-	outstream.write("\n"+str(benchmark[0])+" Daily Sortino Ratio(for year):")         
-	for year in years:
-		year_vals=[]
-		for date in BENCHMARK_CLOSE.index:
-			if(date.year==year):
-				year_vals.append(BENCHMARK_CLOSE.xs(date))
-		ret=fu.getSortinoRatio(year_vals)
-		x="\t\t%+6.2f" % ret
-		outstream.write(x)
+    outstream.write("\n" + str(benchmark[0]) + \
+                " Daily Sortino Ratio(for year):")         
+    for year in years:
+        year_vals = []
+        for date in BENCHMARK_CLOSE.index:
+            if(date.year==year):
+                year_vals.append(BENCHMARK_CLOSE.xs(date))
+        ret = fu.get_sortino_ratio(year_vals)
+        outstream.write("\t\t% + 6.2f" % ret)
 
-	
-	outstream.write("\n\nMonthly Returns %\n\t")
-	month_names=du.getMonthNames()
-	for name in month_names:
-		outstream.write("\t  "+str(name))
-	outstream.write("\n")
-	years=du.getYears(funds)
-	i=0
-	mrets=tsu.monthly(funds)
-	for year in years:
-		outstream.write(str(year)+"\t")
-		months=du.getMonths(funds,year)
-		if(i==0):
-			if(len(months)<12):
-				for k in range(0, 12-len(months)):
-					outstream.write("\t")
-		for month in months:
-			x="\t%+6.2f" % (mrets[i]*100)
-			outstream.write(x)
-			i+=1
-		outstream.write("\n")
-
-
-def print_old_stats(funds, name, html_file):
-	#montly returns
-	mrets=tsu.monthly(funds)
-	html_file.write("<H1>"+str(name)+"</H1>\n")
-	html_file.write("<H2>Monthly Returns</H2>\n")
-	html_file.write("<TABLE CELLPADDING=10>\n")
-	html_file.write("<TR>\n")
-	html_file.write("<TH></TH>\n")
-	month_names=du.getMonthNames()
-	for name in month_names:
-		html_file.write("<TH>"+str(name)+"</TH>\n")
-	html_file.write("</TR>\n")
-	years=du.getYears(funds)
-	i=0
-	for year in years:
-		html_file.write("<TR>\n")
-		html_file.write("<TH>"+str(year)+"</TH>\n")
-		months=du.getMonths(funds,year)
-		for month in months:
-			html_file.write("<TD>"+str(mrets[i]*100)[:4]+"%</TD>\n")
-			i+=1
-		html_file.write("</TR>\n")
-	html_file.write("</TABLE>\n")
-	html_file.write("<BR/>\n\n")
-	
-	#sharpe ratio
-	ratio=tsu.getSharpeRatio(tsu.daily(funds))
-	ratio=0
-	html_file.write("<H3>Overall Sharpe Ratio: "+str(ratio)+"</H3>\n")
-	html_file.write("<TABLE CELLPADDING=10>\n")
-	html_file.write("<TR><TH></TH>\n")
-	for year in years:
-		html_file.write("<TH>"+str(year)+"</TH>\n")
-	html_file.write("</TR>\n")
-	html_file.write("<TR>\n")
-	html_file.write("<TH>Sharpe Ratio:</TH>\n")
-	for year in years:
-		ratio=tsu.getSharpeRatio(funds,year)
-		html_file.write("<TD>"+str(ratio)+"</TD>\n")
-	html_file.write("</TR>\n")
-	html_file.write("</TABLE>\n")
-	html_file.write("<BR/>\n\n")
-	
+    
+    outstream.write("\n\nMonthly Returns %\n\t")
+    month_names = du.getMonthNames()
+    for name in month_names:
+        outstream.write("\t  " + str(name))
+    outstream.write("\n")
+    years = du.getYears(fund_ts)
+    i = 0
+    mrets = tsu.monthly(fund_ts)
+    for year in years:
+        outstream.write(str(year) + "\t")
+        months = du.getMonths(fund_ts, year)
+        if(i==0):
+            if(len(months)<12):
+                for k in range(0, 12-len(months)):
+                    outstream.write("\t")
+        for month in months:
+            outstream.write("\t% + 6.2f" % (mrets[i]*100))
+            i += 1
+        outstream.write("\n")
 
 def generate_report(funds_list, graph_names, out_file):
-	html_file = open("report.html","w")
-	print_header(html_file, out_file)
-	html_file.write("<IMG SRC=\'./funds.png\' width=400/>\n")
-	html_file.write("<BR/>\n\n")
-	i=0
-	plt.clf()
-	#load spx for time frame
-	symbol=["$SPX"]
-	start_date=0
-	end_date=0
-	for fund in funds_list:
-		if(type(fund)!=type(list())):
-			if(start_date==0 or start_date>fund.index[0]):
-				start_date=fund.index[0]	
-			if(end_date==0 or end_date<fund.index[-1]):
-				end_date=fund.index[-1]	
-			mult=10000/fund.values[0]
-			plt.plot(fund.index, fund.values*mult,label=path.basename(graph_names[i]))
-		else:	
-			if(start_date==0 or start_date>fund[0].index[0]):
-				start_date=fund[0].index[0]	
-			if(end_date==0 or end_date<fund[0].index[-1]):
-				end_date=fund[0].index[-1]	
-			mult=10000/fund[0].values[0]
-			plt.plot(fund[0].index, fund[0].values*mult, label=path.basename(graph_names[i]))	
-		i+=1
-	timeofday=dt.timedelta(hours=16)
-	timestamps=du.getNYSEdays(start_date, end_date, timeofday)
-	dataobj=da.DataAccess('Norgate')
-	global BENCHMARK_CLOSE
-	BENCHMARK_CLOSE=dataobj.get_data(timestamps,symbol, "close", verbose=False)
-	mult=10000/BENCHMARK_CLOSE.values[0]
-	i=0
-	for fund in funds_list:
-		if(type(fund)!=type(list())):
-			print_stats(fund, ["$SPX"], graph_names[i])
-		else:	
-			print_stats(fund[0], ["$SPX"], graph_names[i])
-		i+=1
-	plt.plot(BENCHMARK_CLOSE.index,BENCHMARK_CLOSE.values*mult,label="SSPX")
-	plt.ylabel('Fund Value')
-	plt.xlabel('Date')
-	plt.legend()
-	plt.draw()
-	savefig('funds.png', format='png')
-	print_footer(html_file)
+    """
+    @summary generates a report given a list of fund time series
+    """
+    html_file  =  open("report.html","w")
+    print_header(html_file, out_file)
+    html_file.write("<IMG SRC = \'./funds.png\' width = 400/>\n")
+    html_file.write("<BR/>\n\n")
+    i = 0
+    plt.clf()
+    #load spx for time frame
+    symbol = ["$SPX"]
+    start_date = 0
+    end_date = 0
+    for fund in funds_list:
+        if(type(fund)!= type(list())):
+            if(start_date == 0 or start_date>fund.index[0]):
+                start_date = fund.index[0]    
+            if(end_date == 0 or end_date<fund.index[-1]):
+                end_date = fund.index[-1]    
+            mult = 10000/fund.values[0]
+            plt.plot(fund.index, fund.values * mult, label = \
+                                 path.basename(graph_names[i]))
+        else:    
+            if(start_date == 0 or start_date>fund[0].index[0]):
+                start_date = fund[0].index[0]    
+            if(end_date == 0 or end_date<fund[0].index[-1]):
+                end_date = fund[0].index[-1]    
+            mult = 10000/fund[0].values[0]
+            plt.plot(fund[0].index, fund[0].values * mult, label = \
+                                      path.basename(graph_names[i]))    
+        i += 1
+    timeofday = dt.timedelta(hours = 16)
+    timestamps = du.getNYSEdays(start_date, end_date, timeofday)
+    dataobj = da.DataAccess('Norgate')
+    global BENCHMARK_CLOSE
+    BENCHMARK_CLOSE = dataobj.get_data(timestamps, symbol, "close", \
+                                            verbose = False)
+    mult = 10000/BENCHMARK_CLOSE.values[0]
+    i = 0
+    for fund in funds_list:
+        if(type(fund)!= type(list())):
+            print_stats(fund, ["$SPX"], graph_names[i])
+        else:    
+            print_stats( fund[0], ["$SPX"], graph_names[i])
+        i += 1
+    plt.plot(BENCHMARK_CLOSE.index, BENCHMARK_CLOSE.values*mult, label = "SSPX")
+    plt.ylabel('Fund Value')
+    plt.xlabel('Date')
+    plt.legend()
+    plt.draw()
+    savefig('funds.png', format = 'png')
+    print_footer(html_file)
 
-def generate_robust_report(file, out_file):
-	html_file = open(filename,"w")
-	print_header(html_file, out_file)
-	converter.fundsToPNG(funds,'funds.png')
-	html_file.write("<H2>QSTK Generated Report:"+filename+"</H2>\n")
-	html_file.write("<IMG SRC=\'./funds.png\'/>\n")
-	html_file.write("<IMG SRC=\'./analysis.png\'/>\n")
-	html_file.write("<BR/>\n\n")
-	fund=cPickle.load(file)
-	#add funds to graph
-	#add analysis
-	print_stats(fund, html_file)
-	print_footer(html_file)
+def generate_robust_report(fund_matrix, out_file):
+    """
+    @summary generates a report using robust backtesting
+    @param fund_matrix: a pandas matrix of fund time series
+    @param out_file: filename where to print report
+    """
+    html_file  =  open(out_file,"w")
+    print_header(html_file, out_file)
+    converter.fundsToPNG(fund_matrix,'funds.png')
+    html_file.write("<H2>QSTK Generated Report:" + out_file + "</H2>\n")
+    html_file.write("<IMG SRC = \'./funds.png\'/>\n")
+    html_file.write("<IMG SRC = \'./analysis.png\'/>\n")
+    html_file.write("<BR/>\n\n")
+    print_stats(fund_matrix, "robust funds", html_file)
+    print_footer(html_file)
 
-if __name__ == '__main__':
-	# Usage
-	#
-	# Normal:
-	# python report.py 'out.pkl' ['out2.pkl' ...]
-	#
-	# Robust:
-	# python report.py -r 'out.pkl'
-	#
-	
-	robust=0
+if __name__  ==  '__main__':
+    # Usage
+    #
+    # Normal:
+    # python report.py 'out.pkl' ['out2.pkl' ...]
+    #
+    # Robust:
+    # python report.py -r 'out.pkl'
+    #
+    
+    ROBUST = 0
 
-	if(sys.argv[1]=='-r'):
-		robust=1
+    if(sys.argv[1] == '-r'):
+        ROBUST = 1
 
-	filename = "report.html"
-	
-	if(robust==1):
-		input=open(sys.argv[2],"r")
-		funds=cPickle.load(input)
-		generate_robust_report(funds,filename)
-	else:
-		files=sys.argv
-		files.remove(files[0])
-		funds=[]
-		for file in files:
-			input=open(file,"r")
-			fund=cPickle.load(input)
-			funds.append(fund)
-		generate_report(funds, files, filename)
+    FILENAME  =  "report.html"
+    
+    if(ROBUST == 1):
+        ANINPUT = open(sys.argv[2],"r")
+        FUNDS = cPickle.load(ANINPUT)
+        generate_robust_report(FUNDS, FILENAME)
+    else:
+        FILES = sys.argv
+        FILES.remove(FILES[0])
+        FUNDS = []
+        for AFILE in FILES:
+            ANINPUT = open(AFILE,"r")
+            FUND = cPickle.load(ANINPUT)
+            FUNDS.append(FUND)
+        generate_report(FUNDS, FILES, FILENAME)
 
 
--- trunk/qstkutil/fundutil.py
+++ trunk/qstkutil/fundutil.py
@@ -1,36 +1,46 @@
+'''
+(c) 2012 Lucena Research, LLC.  All rights reserved.
+
+Created on 4/18/2012
+
+@author: Drew Bratcher
+@contact: dbratcher@gatech.edu
+@summary: Contains utility functions to create statistics for fund time series.
+'''
+
 import qstkutil.tsutil as tsu
 
-def getWinningDays(fund_ts):
-	"""
-	@summary Returns percentage of winning days in fund time series
-	@param fund_ts: pandas time series of daily fund values
-	@return Percentage of winning days over fund time series
-	"""
-	return tsu.getWinningDays(tsu.daily(fund_ts))
+def get_winning_days(fund_ts):
+    """
+    @summary Returns percentage of winning days in fund time series
+    @param fund_ts: pandas time series of daily fund values
+    @return Percentage of winning days over fund time series
+    """
+    return tsu.get_winning_days(tsu.daily(fund_ts))
 
-def getMaxDrawDown(fund_ts):
-	"""
-	@summary Returns max draw down of fund time series (in percentage)
-	@param fund_ts: pandas time series of daily fund values
-	@return Max draw down of fund time series
-	"""
-	return tsu.getMaxDrawDown(tsu.daily(fund_ts))
+def get_max_draw_down(fund_ts):
+    """
+    @summary Returns max draw down of fund time series (in percentage)
+    @param fund_ts: pandas time series of daily fund values
+    @return Max draw down of fund time series
+    """
+    return tsu.get_max_draw_down(tsu.daily(fund_ts))
 
-def getSortinoRatio(fund_ts):
-	"""
-	@summary Returns daily computed Sortino ratio of fund time series
-	@param fund_ts: pandas time series of daily fund values
-	@return Sortino ratio of fund time series
-	"""
-	return tsu.getSortinoRatio(tsu.daily(fund_ts))
+def get_sortino_ratio(fund_ts):
+    """
+    @summary Returns daily computed Sortino ratio of fund time series
+    @param fund_ts: pandas time series of daily fund values
+    @return Sortino ratio of fund time series
+    """
+    return tsu.get_sortino_ratio(tsu.daily(fund_ts))
 
-def getSharpeRatio(fund_ts):
-	"""
-	@summary Returns daily computed Sharpe ratio of fund time series
-	@param fund_ts: pandas time series of daily fund values
-	@return  Sharpe ratio of  fund time series
-	"""
-	return tsu.getSharpeRatio(tsu.daily(fund_ts))
+def get_sharpe_ratio(fund_ts):
+    """
+    @summary Returns daily computed Sharpe ratio of fund time series
+    @param fund_ts: pandas time series of daily fund values
+    @return  Sharpe ratio of  fund time series
+    """
+    return tsu.get_sharpe_ratio(tsu.daily(fund_ts))
 
 
 
