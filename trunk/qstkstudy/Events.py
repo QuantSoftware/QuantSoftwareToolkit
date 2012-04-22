1ded3247d3e58590b6076ae8b02c416bd178c3d1
vishalShekhar <vishalShekhar@gmail.com>
2012-04-22 21:14:02 +0000

    Added Licence text for BSD release.

--- trunk/qstkstudy/Events.py
+++ trunk/qstkstudy/Events.py
@@ -1,8 +1,15 @@
+# (c) 2011, 2012 Georgia Tech Research Corporation
+# This source code is released under the New BSD license.  Please see
+# http://wiki.quantsoftware.org/index.php?title=QSTK_License
+# for license details.
+
+#Created on October <day>, 2011
 #
-# Find events
-# by Vishal Shekhar
-# October 2011
+#@author: Vishal Shekhar
+#@contact: mailvishalshekhar@gmail.com
+#@summary: Example Event Datamatrix acceptable to EventProfiler App
 #
+
 import pandas 
 from qstkutil import DataAccess as da
 import numpy as np
