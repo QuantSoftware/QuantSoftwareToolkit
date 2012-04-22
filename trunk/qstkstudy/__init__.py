dc131faa7511e827cd7df68ae4077c8c4921f999
vishalShekhar <vishalShekhar@gmail.com>
2012-04-22 21:23:14 +0000

    Added Licence Text for BSD release

---
+++ trunk/qstkstudy/__init__.py
@@ -0,0 +1,10 @@
+# (c) 2011, 2012 Georgia Tech Research Corporation
+# This source code is released under the New BSD license.  Please see
+# http://wiki.quantsoftware.org/index.php?title=QSTK_License
+# for license details.
+#
+# Created on October <day>, 2011
+#
+# @author: Vishal Shekhar
+# @contact: mailvishalshekhar@gmail.com
+# @summary: Init
--- trunk/qstkstudy/stockListGen.py
+++ trunk/qstkstudy/stockListGen.py
@@ -1,3 +1,14 @@
+# (c) 2011, 2012 Georgia Tech Research Corporation
+# This source code is released under the New BSD license.  Please see
+# http://wiki.quantsoftware.org/index.php?title=QSTK_License
+# for license details.
+#
+# Created on October <day>, 2011
+#
+# @author: Vishal Shekhar
+# @contact: mailvishalshekhar@gmail.com
+# @summary: Utiltiy script to create list of symbols for study.
+
 import qstkutil.DataAccess as da
 import qstkutil.dateutil as du
 import datetime as dt
