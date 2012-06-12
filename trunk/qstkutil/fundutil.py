393f89b76f8da6186e178ad3189be69b15eaa6eb
dbratcher <dbratcher@gatech.edu>
2012-06-12 14:47:09 +0000

    fixed max draw down

--- trunk/qstkutil/fundutil.py
+++ trunk/qstkutil/fundutil.py
@@ -28,7 +28,17 @@ def get_max_draw_down(fund_ts):
     @param fund_ts: pandas time series of daily fund values
     @return Max draw down of fund time series
     """
-    return tsu.get_max_draw_down(tsu.daily(fund_ts))
+    MDD = 0
+    DD = 0
+    peak = -99999
+    for value in fund_ts:
+        if (value > peak):
+            peak = value
+        else:
+            DD = (peak - value) / peak
+        if (DD > MDD):
+            MDD = DD
+    return -1*MDD
 
 def get_sortino_ratio(fund_ts):
     """
