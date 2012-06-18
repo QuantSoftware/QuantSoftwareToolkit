0837a50cfa6672bd745dd866b55d94aac9bc40f2
dbratcher <dbratcher@gatech.edu>
2012-06-18 20:11:38 +0000

    updated tutorial 5 to use new tradesim instead of quicklime

--- trunk/Examples/Basic/tutorial5.py
+++ trunk/Examples/Basic/tutorial5.py
@@ -26,29 +26,40 @@ Created on September, 12, 2011
 
 #python imports
 import os
+import pandas as pand
 import datetime as dt
 
 #qstk imports
-from quicksim import quickSim as qs
+import qstksim
+import qstkutil.DataAccess as da
+import qstkutil.qsdateutil as du
 from Bin import report
+        
+startday = dt.datetime(2008, 1, 1)
+endday = dt.datetime(2011, 2, 1)
 
-#STRATegy to use
-STRAT = os.environ['QS']+"/quicksim/STRATegies/OneStock.py"
+l_symbols = ['AAPL']
 
-#start and end dates to start from
-START = dt.datetime(2009,  5, 1)
-END = dt.datetime(2012,  9, 1)
+#Get desired timestamps
+timeofday = dt.timedelta(hours = 16)
+ldt_timestamps = du.getNYSEdays(startday, endday, timeofday)
 
-#number of tests to do and the number of days offset
-NUM = 10
-OFFSET = 1
+dataobj = da.DataAccess('Norgate')
+df_close = dataobj.get_data( \
+                ldt_timestamps, l_symbols, "close", verbose=True)
 
-#starting fund value
-STARTVAL = 10000
+df_alloc = pand.DataFrame(index=[dt.datetime(2009, 2, 1)], data=[1], columns=l_symbols)
 
-#perform tests
+for i in range(11):
+    df_alloc = df_alloc.append( \
+             pand.DataFrame(index=[dt.datetime(2009, i+2, 3)], \
+                              data=[1], columns=l_symbols))
 
-FUNDS = qs.strat_backtest1(STRAT, START, END, 1, OFFSET, STARTVAL)
+df_alloc['_CASH'] = 0.0
 
-report.print_stats(FUNDS[0], ["$SPX"], "FUND TITLE", leverage=FUNDS[0], commissions=25000, slippage=10000, directory="/Users/drewbratcher/QSTK/Examples/Basic/")
-report.print_plot(FUNDS, ["$SPX"], "FUND TITLE", "FundFile.png")
+''' Tests tradesim buy-on-open functionality '''
+(df_funds, ts_leverage, f_commision, f_slippage) = qstksim.tradesim( df_alloc, df_close, 10000, 1, True, 0.02, 5, 0.02)
+
+df_goog = pand.DataFrame(index=df_funds.index, data=df_funds.values, columns=l_symbols)
+print df_goog
+report.print_stats(df_goog, ["$SPX"], "AAPL", leverage=ts_leverage, commissions=f_commision, slippage=f_slippage, directory="./AAPL/")
