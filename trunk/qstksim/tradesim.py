937cced6f86094d32e5771b5869f36b628704c1e
dbratcher <dbratcher@gatech.edu>
2012-06-29 15:29:41 +0000

    latest log

--- trunk/qstksim/tradesim.py
+++ trunk/qstksim/tradesim.py
@@ -105,7 +105,7 @@ def tradesim( alloc, df_historic, f_start_cash, i_leastcount=1,
     #write column headings
     if log!="false":
         print "writing transaction log to "+log
-        log_file.write("Name,Symbol,Type,Date,Shares,Price,Cash value,Share Total,Fund Total,Commission,Notes\n")
+        log_file.write("Symbol,Company Name,Txn Type,Date/Time,# Shares,Txn Price,Txn Cash Value,Result # Shares,Result Value,Commission,Slippage(10BPS),Comments\n")
     
     #a dollar is always worth a dollar
     df_historic['_CASH'] = 1.0
@@ -274,8 +274,8 @@ def tradesim( alloc, df_historic, f_start_cash, i_leastcount=1,
                     if log!="false":
                         if(abs(order[sym])!=0):
                             log_file.write(str(sym) + ","+str(sym)+","+order_type+","+str(prediction_date)+\
-                                       ","+str(abs(order[sym]))+","+str(trade_price[sym].values[0])+","+str(trade_price[sym].values[0]*order[sym])+","\
-                                       +str(shares[sym].ix[-1])+","+str(value_after_trade)+","+str(f_stock_commission)+","+str(round(f_slippage_cost,2)))
+                                       ","+str(order[sym])+","+str(trade_price[sym].values[0])+","+str(trade_price[sym].values[0]*order[sym])+","\
+                                       +str(shares[sym].ix[-1])+","+str(value_after_trade)+","+str(f_stock_commission)+","+str(round(f_slippage_cost,2))+",")
                             log_file.write("\n")
             
             shares['_CASH'] = shares['_CASH'] + cashleft
