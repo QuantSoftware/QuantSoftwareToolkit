43e6d726288d597e32ac76398061dbfb5abe38cc
sb2nov <sb2nov@gmail.com>
2012-08-21 20:00:45 -0400

    deleting the files

--- tags/hdf/qstkmodels/PositionModel.py
+++
@@ -1,8 +0,0 @@
-import tables as pt #@UnresolvedImport
-import numpy as np
-
-class PositionModel(pt.IsDescription):
-    timestamp = pt.Time64Col()
-    symbol = pt.StringCol(30) 
-    shares = pt.Int32Col()
-    purchase_price = pt.Float32Col()
