0a4fa5a81d00197b5bd6f538b96de0282f298580
Sourabh <sb2nov@gmail.com>
2012-10-09 01:21:21 -0400

    Pylint score improvement in the created files

--- Bin/csvformatter.py
+++ Bin/csvformatter.py
@@ -1,28 +1,30 @@
 import csv
-import dateutil.parser as dp
+# import dateutil.parser as dp
 import string
 
-def csv_converter(inputfile, outputfile):
-    actualheader =['Symbol', 'Name', 'Type', 'Date', 'Shares', 'Price', 'Cash value', 'Commission', 'Notes']
 
-    reader=csv.reader(open(inputfile,'r'), delimiter=',')
+def csv_converter(inputfile, outputfile):
+    actualheader = ['Symbol', 'Name', 'Type', 'Date', 'Shares', 'Price', 'Cash value', 'Commission', 'Notes']
+
+    reader = csv.reader(open(inputfile, 'r'), delimiter=',')
     header = reader.next()
+    print "Header : ", header
     input_str = []
     for row in reader:
         input_str.append(row)
     input_str = input_str[::-1]
 
-    writer = csv.writer(open(outputfile,'wb'), delimiter=',')
+    writer = csv.writer(open(outputfile, 'wb'), delimiter=',')
     writer.writerow(actualheader)
 
     for row in input_str[:]:
         # print row
         if row[0] != '--':
             date = row[0]
-        else: 
+        else:
             date = row[1]
 
-        Commission = '0' 
+        Commission = '0'
         Notes = ''
         cashvalue = row[12]
 
@@ -36,7 +38,7 @@ def csv_converter(inputfile, outputfile):
             ordertype = 'Sell'
             symbol = row[9]
             name = row[8]
-            shares = str(int(row[10])*(-1))
+            shares = str(int(row[10]) * (-1))
             price = row[11]
         elif row[7] == 'Purchase ':
             ordertype = 'Buy'
@@ -45,21 +47,21 @@ def csv_converter(inputfile, outputfile):
             shares = row[10]
             price = row[11]
         elif row[7] == 'Dividend' or row[7] == 'Bank Interest' \
-                 or row[7] == 'Interest Charge' or row[7] =='Paymnt In Lieu' \
+                 or row[7] == 'Interest Charge' or row[7] == 'Paymnt In Lieu' \
                  or row[7] == 'Annual Charge' or row[7] == 'Funds Transfer':
             ordertype = 'Deposit Cash'
             symbol = ''
             name = row[7]
             shares = ''
             price = ''
-        elif row[7] == 'Stock Dividend' or row[7]=='DUDB':
+        elif row[7] == 'Stock Dividend' or row[7] == 'DUDB':
             ordertype = 'Buy'
             symbol = row[9]
             name = 'Dividend'
             shares = row[10]
             price = '0'
-        elif row[7] == 'Journal Entry': 
-            if row[10] != '--':     
+        elif row[7] == 'Journal Entry':
+            if row[10] != '--':
                 ordertype = 'Buy'
                 symbol = row[9]
                 name = row[7] + row[8]
@@ -73,7 +75,7 @@ def csv_converter(inputfile, outputfile):
                 price = ''
         else:
             name = row[7] + row[8]
-            if row[10]=='--':
+            if row[10] == '--':
                 shares = ''
                 ordertype = 'Deposit Cash'
                 price = ''
