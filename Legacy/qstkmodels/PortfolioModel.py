b86afd81f9373046fdebe8e7071901ae439c479d
sb2nov <sb2nov@gmail.com>
2013-01-12 19:20:39 -0500

    Deleting old files

--- Legacy/qstkmodels/PortfolioModel.py
+++
@@ -1,5 +0,0 @@
-import tables as pt #@UnresolvedImport
-
-class PortfolioModel(pt.IsDescription):
-    cash = pt.Float32Col()
-    timestamp = pt.Time64Col()
--- csvconverter/cron_script.sh
+++
@@ -1,2 +0,0 @@
-export QSDATA=/hzr71/research/QSData
-/usr/local/bin/python /nethome/sjoshi42/cronjob/csv_to_pkl.py
