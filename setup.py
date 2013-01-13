aa044c78121083c15ab15d8fd2513c4a9b2482bc
sb2nov <sb2nov@gmail.com>
2013-01-12 19:26:47 -0500

    setup.py edited

---
+++ setup.py
@@ -0,0 +1,22 @@
+from distutils.core import setup
+
+setup(
+    name='QSTK',
+    version='0.2.0',
+    author='Sourabh Bajaj',
+    packages=['QSTK'],
+    long_description=open('README.md').read(),
+    author_email='sourabhbajaj90@gmail.com',
+    url='http://wiki.quantsoftware.org',
+    license='LICENSE.txt',
+    description='QuantSoftware Toolkit',
+    install_requires=[
+        "numpy >= 1.6.1",
+        "scipy >= 0.10.0",
+        "matplotlib >= 1.1.0",
+        "pandas==0.7.3",
+        "dateutil >= 1.5",
+        "cvxopt >= 1.1.4",
+        "scikit-learn >= 0.11",
+    ],
+)
