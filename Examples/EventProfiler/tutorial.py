83966ff4c8175f578c734880ec3cf0ce997ef8e3
Sourabh <sb2nov@gmail.com>
2013-02-04 02:36:12 -0500

    fixes in the tutorials

--- Examples/EventProfiler/tutorial.py
+++ Examples/EventProfiler/tutorial.py
@@ -60,17 +60,18 @@ def find_events(ls_symbols, d_data):
 
 
 if __name__ == '__main__':
-    ls_symbols = np.loadtxt('SP500port.csv', dtype='S10', comments='#', skiprows=1)
     dt_start = dt.datetime(2008, 1, 1)
     dt_end = dt.datetime(2009, 12, 31)
     ldt_timestamps = du.getNYSEdays(dt_start, dt_end, dt.timedelta(hours=16))
 
     dataobj = da.DataAccess('Yahoo')
+    ls_symbols = dataobj.get_symbols_from_list('sp5002012')
     ls_keys = ['open', 'high', 'low', 'close', 'volume', 'actual_close']
     ldf_data = dataobj.get_data(ldt_timestamps, ls_symbols, ls_keys)
     d_data = dict(zip(ls_keys, ldf_data))
 
     df_events = find_events(ls_symbols, d_data)
+    print "Creating Study"
     ep.eventprofiler(df_events, d_data, i_lookback=20, i_lookforward=20,
-                s_filename='MyEventStudy', b_market_neutral=True, b_errorbars=True,
+                s_filename='MyEventStudy.pdf', b_market_neutral=True, b_errorbars=True,
                 s_market_sym='SPY')
