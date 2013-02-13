7da5f98ea8a694abeae8d60dcfc0a49f7ec98db6
Sourabh <sb2nov@gmail.com>
2013-02-13 01:44:20 -0500

    install distribure first for easy_install to run

--- MacInstallation.sh
+++ MacInstallation.sh
@@ -15,6 +15,7 @@
 
 echo "Installing dependency - GIT"
 sudo port install git-core +svn
+sudo port install py27-distribute
 echo "Installing dependencies - Numpy Scipy matplotlib"
 sudo port install py27-numpy
 sudo port install py27-scipy
@@ -25,9 +26,8 @@ echo "Installing dependencies - scikits"
 sudo easy_install -U scikit-learn
 echo "Installing dependencies - pandas"
 sudo easy_install pandas==0.7.3
-echo "Installing dependencies - Docs/Distribute"
+echo "Installing dependencies - Docs"
 sudo port install py27-epydoc
-sudo port install py27-distribute
 echo "Installing dependencies - PYQT4"
 sudo port install py27-pyqt4
 echo "Installing dependencies - CVXOPT"
