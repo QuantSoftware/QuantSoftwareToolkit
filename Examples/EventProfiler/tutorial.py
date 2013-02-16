3d88f4683df5cd370820e85544d687465ab9cae0
Sourabh <sb2nov@gmail.com>
2013-02-16 13:09:03 -0500

    new event profiler tutorial

--- Examples/EventProfiler/tutorial.py
+++ Examples/EventProfiler/tutorial.py
@@ -42,6 +42,8 @@ nan = no information about any event.
 def find_events(ls_symbols, d_data):
     ''' Finding the event dataframe '''
     df_close = d_data['close']
+    ts_market = df_close['SPY']
+
     print "Finding Events"
 
     # Creating an empty dataframe
@@ -52,8 +54,18 @@ def find_events(ls_symbols, d_data):
     ldt_timestamps = df_close.index
 
     for s_sym in ls_symbols:
-        for i in range(len(ldt_timestamps)):
-            if df_close[s_sym].ix[ldt_timestamps[i - 1]] >= 5 and df_close[s_sym].ix[ldt_timestamps[i]] < 5:
+        for i in range(1, len(ldt_timestamps)):
+            # Calculating the returns for this timestamp
+            f_symprice_today = df_close[s_sym].ix[ldt_timestamps[i]]
+            f_symprice_yest = df_close[s_sym].ix[ldt_timestamps[i - 1]]
+            f_marketprice_today = ts_market.ix[ldt_timestamps[i]]
+            f_marketprice_yest = ts_market.ix[ldt_timestamps[i - 1]]
+            f_symreturn_today = (f_symprice_today / f_symprice_yest) - 1
+            f_marketreturn_today = (f_marketprice_today / f_marketprice_yest) - 1
+
+            # Event is found if the symbol is down more then 3% while the
+            # market is up more then 2%
+            if f_symreturn_today <= -0.03 and f_marketreturn_today >= 0.02:
                 df_events[s_sym].ix[ldt_timestamps[i]] = 1
 
     return df_events
