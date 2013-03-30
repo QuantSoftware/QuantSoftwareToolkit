e38b1dca7c5c2a222439fafd665ffb62b3935e0b
Sourabh <sb2nov@gmail.com>
2013-03-30 01:40:02 -0400

    fill data

--- Examples/EventProfiler/tutorial.py
+++ Examples/EventProfiler/tutorial.py
@@ -79,9 +79,15 @@ if __name__ == '__main__':
     dataobj = da.DataAccess('Yahoo')
     ls_symbols = dataobj.get_symbols_from_list('sp5002012')
     ls_symbols.append('SPY')
+
     ls_keys = ['open', 'high', 'low', 'close', 'volume', 'actual_close']
     ldf_data = dataobj.get_data(ldt_timestamps, ls_symbols, ls_keys)
     d_data = dict(zip(ls_keys, ldf_data))
+
+    for s_key in ls_keys:
+        d_data[s_key] = d_data[s_key].fillna(method='ffill')
+        d_data[s_key] = d_data[s_key].fillna(method='bfill')
+        d_data[s_key] = d_data[s_key].fillna(1.0)
 
     df_events = find_events(ls_symbols, d_data)
     print "Creating Study"
