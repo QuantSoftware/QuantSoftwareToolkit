08a438725efc59c383866ff4ceb98dd93a48f03f
Sourabh <sb2nov@gmail.com>
2013-07-21 00:04:26 -0400

    Automated scripts for Installation

---
+++ Scripts/AutomatedMac.sh
@@ -0,0 +1,58 @@
+#
+# (c) 2011, 2012 Georgia Tech Research Corporation
+# This source code is released under the New BSD license.  Please see
+# http://wiki.quantsoftware.org/index.php?title=QSTK_License
+# for license details.
+
+# Created on Jan 16, 2013
+
+# @author: Sourabh Bajaj
+# @contact: sourabhbajaj90@gmail.com
+# @summary: Automated Mac Installation script of QSTK
+#
+
+echo "Install Homebrew"
+
+ruby -e "$(curl -fsSL https://raw.github.com/mxcl/homebrew/go)"
+echo 'export PATH="/usr/local/bin:$PATH"' >> ~/.bash_profile
+brew doctor
+
+echo "Install QSTK"
+echo "Installing python"
+brew install python
+
+brew tap samueljohn/python
+brew tap homebrew/science
+
+echo "virtualenv"
+pip install virtualenv
+pip install nose
+
+echo "Installing gfortran"
+brew install gfortran
+
+echo "Installing numpy, scipy, matplotlib"
+brew install numpy
+brew install scipy
+brew install matplotlib
+
+echo "Create a virtual env"
+mkdir ~/QSTK
+cd ~/QSTK
+virtualenv env --distribute --system-site-packages
+source ~/QSTK/env/bin/activate
+
+echo "Install pandas, scikits"
+pip install pandas
+pip install scikits.statsmodels
+pip install scikit-learn
+pip install QSTK
+
+echo "Test QSTK"
+cd ~/QSTK
+curl -O https://spark-public.s3.amazonaws.com/compinvesting1/QSTK-Setups/Examples.zip
+unzip Examples.zip
+python Validation.py
+
+echo "Deactivating"
+deactivate
---
+++ Scripts/AutomatedUbuntu.sh
@@ -0,0 +1,52 @@
+#
+# (c) 2011, 2012 Georgia Tech Research Corporation
+# This source code is released under the New BSD license.  Please see
+# http://wiki.quantsoftware.org/index.php?title=QSTK_License
+# for license details.
+
+# Created on Jan 16, 2013
+
+# @author: Sourabh Bajaj
+# @contact: sourabhbajaj90@gmail.com
+# @summary: Automated Ubuntu Installation script of QSTK
+#
+
+echo "Updating apt-get"
+sudo apt-get update
+# sudo apt-get upgrade
+# echo "Installing dependency - GIT"
+# sudo apt-get install git-core
+echo "Installing dependencies - Numpy Scipy matplotlib"
+sudo apt-get install python-numpy
+sudo apt-get install python-scipy
+sudo apt-get install python-matplotlib
+echo "Installing dependencies - developer tools"
+sudo apt-get install python-dev
+sudo apt-get install python-setuptools
+sudo apt-get install python-pip
+echo "Installing dependencies - scikits"
+sudo pip install scikits.statsmodels
+sudo pip install scikit-learn
+echo "Installing dependencies - pandas"
+sudo pip install pandas
+# echo "Installing dependencies - PYQT4"
+# sudo apt-get install python-qt4
+echo "Installing dependencies - CVXOPT"
+sudo apt-get build-dep python-cvxopt
+sudo apt-get install python-cvxopt
+
+echo "Installing QSTK"
+sudo pip install QSTK
+
+echo "Install Unzip"
+sudo apt-get install unzip
+
+echo "Examples and Validation"
+mkdir ~/QSTK
+cd ~/QSTK
+
+wget https://spark-public.s3.amazonaws.com/compinvesting1/QSTK-Setups/Examples.zip
+unzip Examples.zip
+
+echo "Validating Installation"
+python Validation.py
